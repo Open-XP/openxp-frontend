e6a417b850bcd4fe2e2e6e98913b5fb7bc16c3ba
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-28 10:29:15 +0100

    feat: Add DashboardNavBar component to DashboardSideBar

---
+++ src/components/DashBoardSideBar/DashboardNavBar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BellIcon, MoonIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import { ReactComponent as OpenxpSVG } from "../../svgs/openxp.svg";
+
+export function DashboardNavBar() {
+  return (
+    <div className="flex fixed w-full py-4 px-4 justify-between items-center shadow-xl shadow-blue-gray-900/5 font-semibold bg-white">
+      <div className="flex ml-10 gap-2">
+        <OpenxpSVG className="w-16 h-16 m-auto" />
+        <div className=" text-3xl py-4">openxp</div>
+      </div>
+
+      <input
+        placeholder="What do you want to learn"
+        className="w-1/4 h-16 pl-8 rounded-3xl bg-purple-900 placeholder-white text-white text-lg outline-none"
+      ></input>
+      <div className="flex gap-8">
+        <div className="w-16 h-16" style={{ transform: "rotate(15deg)" }}>
+          <MoonIcon />
+        </div>
+
+        <div className="flex h-16 w-20">
+          <BellIcon />
+          <div className="w-6 h-6 bg-blue-600 absolute ml-8 rounded-full text-white text-center">
+            3
+          </div>
+        </div>
+        <div className="w-16 h-16">
+          <UserCircleIcon />
+        </div>
+      </div>
+      <button className="w-56 p-4 text-center font-semibold text-2xl text-white rounded-full bg-green-600">
+        Start Drill
+      </button>
+    </div>
+  );
+}
+
+export default DashboardNavBar;
