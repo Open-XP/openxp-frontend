1a76eab553429b880b0a05dde22bee992f7d73bc
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-05-30 11:34:55 +0100

    refactor: Update import paths for Evaluation, Quiz, SideBar, DashboardNavBar, and DashboardHome components

--- src/components/Dashboard/EvaluationScreen/Evaluation.js
+++ src/components/Dashboard/EvaluationScreen/Evaluation.js
@@ -1,9 +1,100 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouterHooks } from "../../../withRouters/withRoutersHook";
+import {
+  fetchSubjectQuestions, // Assuming this fetches all questions for a subject
+  fetchIndividualSubjectQuestion, // Assuming this fetches details for a single question
+} from "../../../Actions/Quiz";
 
 class Evaluation extends Component {
+  static propTypes = {
+    fetchSubjectQuestions: PropTypes.func.isRequired,
+    fetchIndividualSubjectQuestion: PropTypes.func.isRequired,
+    questions: PropTypes.array,
+    question: PropTypes.object,
+    testinstance: PropTypes.object.isRequired, // Making sure testinstance is marked as required
+  };
+
+  state = {
+    currentQuestionIndex: 0,
+    selectedOption: null,
+  };
+
+  componentDidMount() {
+    const { fetchSubjectQuestions, testinstance } = this.props;
+    if (testinstance && testinstance.subject) {
+      fetchSubjectQuestions(testinstance.subject, testinstance.year);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // Fetch individual question when moving to next or previous
+    const { currentQuestionIndex } = this.state;
+    if (currentQuestionIndex !== prevState.currentQuestionIndex) {
+      const { fetchIndividualSubjectQuestion, questions } = this.props;
+      if (questions.length > 0) {
+        fetchIndividualSubjectQuestion(
+          questions[currentQuestionIndex].id // Assuming each question has a unique ID
+        );
+      }
+    }
+  }
+
+  handleNextQuestion = () => {
+    const { currentQuestionIndex } = this.state;
+    const { questions } = this.props;
+    if (currentQuestionIndex < questions.length - 1) {
+      this.setState({
+        currentQuestionIndex: currentQuestionIndex + 1,
+        selectedOption: null,
+      });
+    }
+  };
+
+  handlePreviousQuestion = () => {
+    this.setState((prevState) => ({
+      currentQuestionIndex: Math.max(0, prevState.currentQuestionIndex - 1),
+      selectedOption: null,
+    }));
+  };
+
+  handleOptionChange = (option) => {
+    this.setState({ selectedOption: option });
+  };
+
+  renderQuestion = () => {
+    const { question } = this.props;
+    if (!question) {
+      return <div>Loading question...</div>;
+    }
+
+    return (
+      <div className="flex flex-col gap-4 mt-4">
+        <div className="font-[500] h-fit text-[2.25rem]">{question.text}</div>
+        {question.options.map((option, index) => (
+          <div key={index} className="flex items-center gap-4">
+            <input
+              type="radio"
+              name="option"
+              className="w-[1.938rem] h-[1.938rem]"
+              checked={this.state.selectedOption === option}
+              onChange={() => this.handleOptionChange(option)}
+            />
+            <div className="font-[600] text-[2rem]">{option}</div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   render() {
+    const { currentQuestionIndex, questions } = this.props; // Assuming questions array is in props
+    const totalQuestions = questions ? questions.length : 0;
+    const question = questions[currentQuestionIndex]; // Make sure to extract the current question like this
+
     return (
       <div className="flex flex-col w-screen h-screen -m-[9.9rem]">
         <div className="flex w-screen justify-center shadow-xl">
@@ -15,7 +106,8 @@ class Evaluation extends Component {
                 </button>
               </Link>
               <div className="font-[700] text-[1.5rem]">
-                Mathematics Question: 2010
+                {question && question.subject} Question:{" "}
+                {question && question.year}
               </div>
             </div>
             <div className="flex text-[#2D9CDB] text-[1.5rem] font-[700] items-center">
@@ -26,39 +118,22 @@ class Evaluation extends Component {
         <div className="flex w-screen justify-center mt-[8.25rem]">
           <div className="flex flex-col w-[80%] mt-[rem] gap-4">
             <div className="font-[400] text-[#3C3C3C] text-[1.5rem]">
-              Question 1/60
+              Question {currentQuestionIndex + 1}/{totalQuestions}
             </div>
-            <div className="font-[500] h-fit text-[2.25rem]">
-              If the sum of the interior angles of a polygon is 1080°, how many
-              sides does the polygon have?
-            </div>
-            <div className="flex flex-col gap-4 mt-4">
-              <div className="flex items-center gap-4">
-                <input type="radio" className="w-[1.938rem] h-[1.938rem]" />
-                <div className="font-[600] text-[2rem]">3</div>
-              </div>
-              <div className="flex items-center gap-4">
-                <input type="radio" className="w-[1.938rem] h-[1.938rem]" />
-                <div className="font-[600] text-[2rem]">4</div>
-              </div>
-              <div className="flex items-center gap-4">
-                <input type="radio" className="w-[1.938rem] h-[1.938rem]" />
-                <div className="font-[600] text-[2rem]">5</div>
-              </div>
-              <div className="flex items-center gap-4">
-                <input type="radio" className="w-[1.938rem] h-[1.938rem]" />
-                <div className="font-[600] text-[2rem]">8</div>
-              </div>
-              <div className="flex items-center gap-4">
-                <input type="radio" className="w-[1.938rem] h-[1.938rem]" />
-                <div className="font-[600] text-[2rem]">6</div>
-              </div>
-            </div>
+            {this.renderQuestion()}
             <div className="flex justify-between pt-[3rem]">
-              <button className="bg-[#281266] w-[12.25rem] h-[4.5rem] rounded-lg font-[700] text-white text-[1.5rem]">
+              <button
+                className="bg-[#281266] w-[12.25rem] h-[4.5rem] rounded-lg font-[700] text-white text-[1.5rem]"
+                onClick={this.handlePreviousQuestion}
+                disabled={currentQuestionIndex === 0}
+              >
                 Previous
               </button>
-              <button className="bg-[#BBE6FF] w-[12.25rem] h-[4.5rem] rounded-lg font-[700] text-[1.5rem]">
+              <button
+                className="bg-[#BBE6FF] w-[12.25rem] h-[4.5rem] rounded-lg font-[700] text-[1.5rem]"
+                onClick={this.handleNextQuestion}
+                disabled={currentQuestionIndex === totalQuestions - 1}
+              >
                 Next
               </button>
             </div>
@@ -69,4 +144,17 @@ class Evaluation extends Component {
   }
 }
 
-export default Evaluation;
+const mapStateToProps = (state) => ({
+  questions: state.quiz.questions,
+  question: state.quiz.question,
+  testinstance: state.quiz.testinstance,
+});
+
+const mapDispatchToProps = {
+  fetchSubjectQuestions,
+  fetchIndividualSubjectQuestion,
+};
+
+export default withRouterHooks(
+  connect(mapStateToProps, mapDispatchToProps)(Evaluation)
+);
