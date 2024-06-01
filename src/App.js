942f8e18fe297a5eda184945dd546c81f978245e
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-01 19:43:47 +0100

    refactor: Update import paths for withRouterHooks, Evaluation, Quiz, SideBar, DashboardNavBar, DashboardHome, and ExamSetter components

--- src/App.js
+++ src/App.js
@@ -16,8 +16,6 @@ import NewsletterSectionImg from "./components/NewsletterSectionImg/NewsletterSe
 import Footer from "./components/Footer/Footer.js";
 import FooterImg from "./components/FooterImg/FooterImg.js";
 import LogInPage from "./Pages/Accounts/LogInPage.js";
-import { Provider } from "react-redux";
-import store from "./Store.js";
 import SignUpPage from "./Pages/Accounts/SignUpPage.js";
 import PrivateRoute from "./Pages/DashBoard/PrivateRoute.js";
 import Dashboard from "./Pages/DashBoard/DashBoard.js";
@@ -25,47 +23,45 @@ import NewDashBoard from "./Pages/DashBoard/NewDashBoard.js";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <NavBar />
-                  <Heros />
-                  <OverLay />
-                  <SecondSection />
-                  <QuoteSection />
-                  <DivisionalSection />
-                  <CourseCardImg />
-                  <CourseCard />
-                  <CommunitySection />
-                  <TestimonialSection />
-                  <NewsletterSectionImg />
-                  <NewsletterSection />
-                  <FooterImg />
-                  <Footer />
-                </>
-              }
-            />
-            <Route path="/login" element={<LogInPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateRoute>
-                  <NewDashBoard />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/mock-test" element={<div>Mock Test Page</div>} />
-            <Route path="/dashboard/*" element={<NewDashBoard />} />
-          </Routes>
-        </div>
-      </Router>
-    </Provider>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <NavBar />
+                <Heros />
+                <OverLay />
+                <SecondSection />
+                <QuoteSection />
+                <DivisionalSection />
+                <CourseCardImg />
+                <CourseCard />
+                <CommunitySection />
+                <TestimonialSection />
+                <NewsletterSectionImg />
+                <NewsletterSection />
+                <FooterImg />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/login" element={<LogInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <NewDashBoard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/mock-test" element={<div>Mock Test Page</div>} />
+          <Route path="/dashboard/*" element={<NewDashBoard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
