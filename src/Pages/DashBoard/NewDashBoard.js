4bf7947eccbb28d5cf26724688765084868a6695
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-01 20:24:13 +0100

    refactor: Update import paths for Evaluation component

--- src/Pages/DashBoard/NewDashBoard.js
+++ src/Pages/DashBoard/NewDashBoard.js
@@ -11,7 +11,9 @@ const NewDashBoard = () => {
   const location = useLocation();
 
   // Check if the current path includes "evaluation"
-  const isEvaluationRoute = location.pathname.includes("evaluation");
+  const isEvaluationRoute = location.pathname.includes(
+    "/mock-exam/evaluation/"
+  );
 
   return (
     <div className="m-0">
@@ -22,7 +24,7 @@ const NewDashBoard = () => {
           <Routes>
             <Route path="/" element={<DashboardContent />} />
             <Route path="mock-exam" element={<MockExam />} />
-            <Route path="mock-exam/evaluation/:id" element={<Evaluation />} />
+            <Route path="/mock-exam/evaluation/:id" element={<Evaluation />} />
             {/* Add more dashboard specific routes here */}
           </Routes>
         </div>
