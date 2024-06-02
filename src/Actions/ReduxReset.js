bbaa849cc8d12dde08d0dd6aaada9d5750ef5e39
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-02 12:58:16 +0100

    refactor: Remove ReduxReset action and reducer for resetting state on leaving the page

--- src/Actions/ReduxReset.js
+++
@@ -1,8 +0,0 @@
-import RESET_STATE_ON_LEAVE_PAGE from "./Types";
-
-// Action to reset the state on leaving the page
-export const resetStateOnLeavePage = () => (dispatch) => {
-  dispatch({
-    type: RESET_STATE_ON_LEAVE_PAGE,
-  });
-};
