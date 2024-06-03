5d9c48cb9a78d46df66367ec8123c6cf28d32375
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-03 18:51:03 +0100

    refactor: Update import paths for LandingPage, LogInPage, SignUpPage, and DashBoard components

--- src/App.js
+++ src/App.js
@@ -1,64 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Import components
-import NavBar from "./components/NavBar/NavBar.js";
-import Heros from "./components/Heros/Heros.js";
-import OverLay from "./components/OverLay/Overlay.js";
-import SecondSection from "./components/SecondSection/SecondSection.js";
-import QuoteSection from "./components/QuoteSection/QuoteSection.js";
-import DivisionalSection from "./components/DivisionalSection/DivisionalSection.js";
-import CourseCard from "./components/CourseCard/CourseCard.js";
-import CourseCardImg from "./components/CourseCardImg/CourseCardImg.js";
-import CommunitySection from "./components/CommunitySection/CommunitySection.js";
-import TestimonialSection from "./components/TestimonialSection/TestimonialSection.js";
-import NewsletterSection from "./components/NewsletterSection/NewsletterSection.js";
-import NewsletterSectionImg from "./components/NewsletterSectionImg/NewsletterSectionImg.js";
-import Footer from "./components/Footer/Footer.js";
-import FooterImg from "./components/FooterImg/FooterImg.js";
+import LandingPage from "./components/LandingPage/LandingPage.js";
 import LogInPage from "./Pages/Accounts/LogInPage.js";
 import SignUpPage from "./Pages/Accounts/SignUpPage.js";
 import PrivateRoute from "./Pages/DashBoard/PrivateRoute.js";
-import Dashboard from "./Pages/DashBoard/DashBoard.js";
-import NewDashBoard from "./Pages/DashBoard/NewDashBoard.js";
+import DashBoard from "./Pages/DashBoard/DashBoard.js";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <NavBar />
-                <Heros />
-                <OverLay />
-                <SecondSection />
-                <QuoteSection />
-                <DivisionalSection />
-                <CourseCardImg />
-                <CourseCard />
-                <CommunitySection />
-                <TestimonialSection />
-                <NewsletterSectionImg />
-                <NewsletterSection />
-                <FooterImg />
-                <Footer />
-              </>
-            }
-          />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LogInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route
-            path="/dashboard"
+            path="/dashboard/*"
             element={
               <PrivateRoute>
-                <NewDashBoard />
+                <DashBoard />
               </PrivateRoute>
             }
           />
-          <Route path="/mock-test" element={<div>Mock Test Page</div>} />
-          <Route path="/dashboard/*" element={<NewDashBoard />} />
+          {/* <Route path="/mock-test" element={<div>Mock Test Page</div>} />
+          <Route path="/dashboard/*" element={<DashBoard />} /> */}
         </Routes>
       </div>
     </Router>
