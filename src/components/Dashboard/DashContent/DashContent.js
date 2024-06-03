3ac3cf70084c3eb80b89ecb16635f5a25b5d25d6
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-03 18:55:31 +0100

    refactor: Add DashContent component to Dashboard

---
+++ src/components/Dashboard/DashContent/DashContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DashoardContent from "./DashboardContent/DashboardContent";
+
+export function DashContent() {
+  return (
+    <div>
+      <DashoardContent />
+    </div>
+  );
+}
+
+export default DashContent;
