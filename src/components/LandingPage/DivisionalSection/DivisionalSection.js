4e673f855a727d74c020549273fddd8889b51e05
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-03 18:59:44 +0100

    refactor: Remove unused CSS code from DivisionalSection component

---
+++ src/components/LandingPage/DivisionalSection/DivisionalSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./DivisionalSection.css";
+
+const DivisionalSection = () => {
+  return (
+    <div className="d-s-row">
+      <div className="d-s-bg"></div>
+    </div>
+  );
+};
+
+export default DivisionalSection;
