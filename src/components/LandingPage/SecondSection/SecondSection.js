02060ba6cd420f00f3cafd95de56a6de21b6715a
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-03 19:03:17 +0100

    feat: Add SecondSection component to landing page
    This commit adds the SecondSection component to the landing page. The SecondSection displays information about real-time exam simulation and resources, along with popular examination questions. This component enhances the overall content and visual appeal of the landing page.

---
+++ src/components/LandingPage/SecondSection/SecondSection.js
@@ -0,0 +1,142 @@
+import React from "react";
+import "./SecondSection.css";
+
+const SecondSection = () => {
+  return (
+    <div className="Second-section">
+      <div className="Second-section-bg">
+        <div className="s-s-row row">
+          <div className="col s-s-col-1">
+            <div className="s-s-1">
+              <div className="s-s-text-1">
+                <div className="s-s-screen-icon">
+                  <img
+                    id="s-s-cleanup"
+                    src={require("../../../icons/screen.png")}
+                  />
+                </div>
+                <p className="s-s-font-control-3">
+                  Real time Exam Simulation and Resources.
+                </p>
+              </div>
+              <div className="s-s-text-2">
+                <p className="s-s-font-control-2">
+                  Practice Popular Examination Questions Guranteed to Aid you
+                  Ace your Forthcoming Exams.
+                </p>
+              </div>
+              <div className="s-s-text-3-list">
+                <div id="s-s-cleanup">
+                  <div className="s-s-checked-shield">
+                    <img
+                      id="s-s-icon"
+                      src={require("../../../icons/shield-check.png")}
+                    />
+                  </div>
+                  <p id="s-s-font-control">
+                    Your one Stop Solution to Exam Axiety
+                  </p>
+                </div>
+                <div id="s-s-cleanup">
+                  <div className="s-s-checked-shield">
+                    <img
+                      id="s-s-icon"
+                      src={require("../../../icons/shield-check.png")}
+                    />
+                  </div>
+                  <p id="s-s-font-control">
+                    Your one Stop Solution to Exam Axiety
+                  </p>
+                </div>
+                <div id="s-s-cleanup">
+                  <div className="s-s-checked-shield">
+                    <img
+                      id="s-s-icon"
+                      src={require("../../../icons/shield-check.png")}
+                    />
+                  </div>
+                  <p id="s-s-font-control">
+                    Your one Stop Solution to Exam Axiety
+                  </p>
+                </div>
+                <div id="s-s-cleanup">
+                  <div className="s-s-checked-shield">
+                    <img
+                      id="s-s-icon"
+                      src={require("../../../icons/shield-check.png")}
+                    />
+                  </div>
+                  <p id="s-s-font-control">
+                    Your one Stop Solution to Exam Axiety
+                  </p>
+                </div>
+                <div id="s-s-cleanup">
+                  <div className="s-s-checked-shield">
+                    <img
+                      id="s-s-icon"
+                      src={require("../../../icons/shield-check.png")}
+                    />
+                  </div>
+                  <p id="s-s-font-control">
+                    Your one Stop Solution to Exam Axiety
+                  </p>
+                </div>
+                <div className="s-s-button-container">
+                  <button className="s-s-button-1">
+                    <p>Exam Library</p>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="s-s-2">
+              <div className="s-s-container-bg">
+                <div id="s-s-container">
+                  <img src={require("../../../icons/TOEFL.png")} />
+                  <p id="s-s-paragraph">
+                    TOEFL Examination Qustions and Resources
+                  </p>
+                </div>
+              </div>
+              <div className="s-s-container-bg">
+                <div id="s-s-container">
+                  <img src={require("../../../icons/TOEFL.png")} />
+                  <p id="s-s-paragraph">
+                    TOEFL Examination Questions and Resources
+                  </p>
+                </div>
+              </div>
+              <div className="s-s-container-bg">
+                <div id="s-s-container">
+                  <img src={require("../../../icons/TOEFL.png")} />
+                  <p id="s-s-paragraph">
+                    TOEFL Examination Questions and Resources
+                  </p>
+                </div>
+              </div>
+              <div className="s-s-container-bg">
+                <div id="s-s-container">
+                  <img src={require("../../../icons/TOEFL.png")} />
+                  <p id="s-s-paragraph">
+                    TOEFL Examination Questions and Resources
+                  </p>
+                </div>
+              </div>
+              <div className="s-s-container-bg">
+                <div id="s-s-container">
+                  <img src={require("../../../icons/TOEFL.png")} />
+                  <p id="s-s-paragraph">
+                    TOEFL Examination Questions and Resources
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondSection;
