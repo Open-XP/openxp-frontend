1491845ef2ee46a7f7a1ce64b3c67c6b98b6315e
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-03 18:58:07 +0100

    refactor: Remove unused components and related CSS code

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,37 @@
+import NavBar from "./NavBar/NavBar.js";
+import Heros from "./Heros/Heros.js";
+import OverLay from "./OverLay/Overlay.js";
+import SecondSection from "./SecondSection/SecondSection.js";
+import QuoteSection from "./QuoteSection/QuoteSection.js";
+import DivisionalSection from "./DivisionalSection/DivisionalSection.js";
+import CourseCard from "./CourseCard/CourseCard.js";
+import CourseCardImg from "./CourseCardImg/CourseCardImg.js";
+import CommunitySection from "./CommunitySection/CommunitySection.js";
+import TestimonialSection from "./TestimonialSection/TestimonialSection.js";
+import NewsletterSection from "./NewsletterSection/NewsletterSection.js";
+import NewsletterSectionImg from "./NewsletterSectionImg/NewsletterSectionImg.js";
+import Footer from "./Footer/Footer.js";
+import FooterImg from "./FooterImg/FooterImg.js";
+
+function LandingPage() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Heros />
+      <OverLay />
+      <SecondSection />
+      <QuoteSection />
+      <DivisionalSection />
+      <CourseCardImg />
+      <CourseCard />
+      <CommunitySection />
+      <TestimonialSection />
+      <NewsletterSectionImg />
+      <NewsletterSection />
+      <FooterImg />
+      <Footer />
+    </div>
+  );
+}
+
+export default LandingPage;
