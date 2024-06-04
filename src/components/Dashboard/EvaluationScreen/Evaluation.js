3a128b142f695e2cbdf4acf22e94ae6a003822bf
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-04 12:41:38 +0100

    refactor: Remove unused react-router-dom import from Evaluation.js

--- src/components/Dashboard/EvaluationScreen/Evaluation.js
+++ src/components/Dashboard/EvaluationScreen/Evaluation.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
