2915dfef59a54f60a390641a7c3e17f29e1f9d05
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-05 23:59:03 +0100

    refactor: Removed unwanted comments

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import LogInPage from "./Pages/Accounts/LogInPage.js";
 import SignUpPage from "./Pages/Accounts/SignUpPage.js";
 import PrivateRoute from "./Pages/DashBoard/PrivateRoute.js";
 import DashBoard from "./Pages/DashBoard/DashBoard.js";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+    <div>
+      <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LogInPage />} />
@@ -23,11 +24,9 @@ function App() {
               </PrivateRoute>
             }
           />
-          {/* <Route path="/mock-test" element={<div>Mock Test Page</div>} />
-          <Route path="/dashboard/*" element={<DashBoard />} /> */}
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
