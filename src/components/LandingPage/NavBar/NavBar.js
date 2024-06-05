9b628b297d238c9b318345d77e31321d19ab4a6f
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-05 14:41:53 +0100

    refactor: Remove unused code and dependencies

--- src/components/LandingPage/NavBar/NavBar.js
+++ src/components/LandingPage/NavBar/NavBar.js
@@ -1,59 +1,358 @@
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Dropdown from "react-bootstrap/Dropdown";
-import "./NavBar.css";
+import OpenxpSVG from "../../../svgs/openxp.js";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Fragment } from "react";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from "@headlessui/react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Navbar = () => {
   return (
-    <div className="my-nav-bar">
-      <Link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        rel="stylesheet"
-      />
-      <div className="nav-bg row">
-        <div className="navigation-logo-div col">
-          <Link to={"./"} className="logo no-highlight">
-            <h1 className="">OpenXP</h1>
-          </Link>
+    <div className="flex fixed w-full justify-center bg-[#281266] mix-blend-multiply z-10">
+      <div className="flex w-[90rem] justify-between bg-[#281266] p-4 items-center">
+        <div className="">
+          <div className="flex items-center gap-2">
+            <OpenxpSVG className="w-16 h-16 text-white" />
+            <div className="text-3xl py-4 text-white font-[700] font-sans">
+              openxp
+            </div>
+          </div>
         </div>
-
-        <div className="search col">
+        <div className="w-[23rem] h-[3.25rem] items-center flex">
+          <MagnifyingGlassIcon className="w-10 h-6 text-white absolute pl-3" />
           <input
-            className="searching text-white"
+            className="w-full h-full pl-12 rounded-3xl bg-[#FFFFFF33]/25 border-none text-white text-lg outline-none placeholder-white"
             type="text"
             placeholder="what do you want to learn?"
           />
-          <div className="search-icon">
-            <i className="fas fa-search fa-2x"></i>
-          </div>
         </div>
-
-        <div className="nav-list-item col">
-          <ul>
-            <li id="list-item" style={{ height: "35px" }}>
-              <Link className="no-highlight">Courses</Link>
-              <div className="nav-dropdown"></div>
-            </li>
-            <li id="list-item">
-              <Link className="no-highlight">Resources</Link>
-            </li>
-            <li id="list-item">
-              <Link className="no-highlight">About us</Link>
-            </li>
-            <div className="nav-line"></div>
-            <li id="list-item">
-              <Link to="/dashboard" className="no-highlight">
+        <div className="flex w-[35rem] justify-center">
+          <div className="flex flex-row w-full gap-3 justify-around items-center px-3">
+            <div className="">
+              <Menu>
+                <MenuButton className="flex no-underline text-white font-[400] text-[1rem] leading-[1.4rem] gap-2">
+                  Courses
+                  <ChevronDownIcon
+                    className="-mr-1 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </MenuButton>
+                <Transition
+                  as="div"
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <MenuItems className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-56 origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Edit
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </Transition>
+              </Menu>
+            </div>
+            <div>
+              <Menu>
+                <MenuButton className="flex no-underline text-white font-[400] text-[1rem] leading-[1.4rem] gap-2">
+                  Resources
+                  <ChevronDownIcon
+                    className="-mr-1 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </MenuButton>
+                <Transition
+                  as="div"
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <MenuItems className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-56 origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Edit
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </Transition>
+              </Menu>
+            </div>
+            <div>
+              <Menu>
+                <MenuButton className="flex no-underline text-white font-[400] text-[1rem] leading-[1.4rem] gap-2">
+                  About us
+                  <ChevronDownIcon
+                    className="-mr-1 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </MenuButton>
+                <Transition
+                  as="div"
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <MenuItems className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-56 origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Edit
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Delete
+                          </a>
+                        )}
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </Transition>
+              </Menu>
+            </div>
+            <div className="h-14 w-[1px] bg-white font-[400] text-[1rem] leading-[1.4rem]"></div>
+            <div id="no-underline text-white font-[400] text-[1rem] leading-[1.4rem]">
+              <Link to="/dashboard" className="no-underline text-white">
                 LogIn
               </Link>
-            </li>
-            <li id="list-item">
-              <div className="box">
-                <Link to="/signup" className="no-highlight box-text">
-                  Start Free
-                </Link>
-              </div>
-            </li>
-          </ul>
+            </div>
+            <div className="flex justify-center items-center w-[8rem] h-[3rem] rounded-lg border border-white">
+              <Link
+                to="/signup"
+                className="no-underline text-white items-center"
+              >
+                Start Free
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
