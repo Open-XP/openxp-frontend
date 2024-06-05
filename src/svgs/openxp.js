838129628833f09477e35796cfc044eeef11c2d6
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-05 14:42:12 +0100

    refactor: Remove unused OpenxpSVG component and related code

---
+++ src/svgs/openxp.js
@@ -0,0 +1,17 @@
+const OpenxpSVG = (props) => (
+  <svg
+    width="132"
+    height="146"
+    viewBox="0 0 132 146"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M15.5 118.5L0 109.5V36L66.5 0L132 36V109.5L66.5 146L31 125V53.5L66.5 34L103 55V93L66.5 112L60 108V70.5L66.5 67.5L73 72V91.5L87 84V60.5L66.5 51L46 63V116L69 128.5L119 100V46.5L66.5 17L15.5 46.5V118.5Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default OpenxpSVG;
