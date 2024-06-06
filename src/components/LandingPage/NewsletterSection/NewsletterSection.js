ff705be5645a56f526387b0b9ef92a359cf3f6e9
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-06 23:23:59 +0100

    refactor: Update NewsletterSection component layout and styles

--- src/components/LandingPage/NewsletterSection/NewsletterSection.js
+++ src/components/LandingPage/NewsletterSection/NewsletterSection.js
@@ -1,32 +1,26 @@
 import React from "react";
-import "./NewsletterSection.css";
-import { Button } from "bootstrap";
 
 const NewsletterSection = () => {
   return (
-    <div className="n-s-row">
-      <div className="n-s-bg">
-        <div className="n-s-text-1">
-          <div className="n-s-text-1-1">
-            <p>Subscribe To Our Newsletter</p>
+    <div className="flex w-full h-[36.875rem] justify-center z-20">
+      <div className="flex flex-col w-[90rem] h-[36.875rem] justify-center items-center gap-[4rem]">
+        <div className="w-[64.813rem] h-[11.125rem]">
+          <div className="font-[700] text-[2.5rem] leading-[3.404rem] text-center text-[#281266] font-sans">
+            Subscribe To Our Newsletter
           </div>
-          <div className="n-s-text-1-2">
-            <p>
-              Get news and information for any examination problem and questions
-              you may have
-            </p>
+          <div className="font-[500] text-[2rem] leading-[2.724rem] text-center">
+            Get news and information for any examination problem and <br />{" "}
+            questions you may have
           </div>
         </div>
-        <div className="n-s-text-2">
-          <div className="n-s-text-2-1">
-            <input
-              className="n-s-placeholder"
-              placeholder="Johndoe@gmail.com"
-            />
-          </div>
-          <div className="n-s-text-2-2">
-            <button className="n-s-button">Subscribe Now</button>
-          </div>
+        <div className="flex">
+          <input
+            className="w-[50.813rem] h-[5rem] rounded-l-[3.438rem] px-[5rem] placeholder-white bg-[#281266] border-none font-sans font-[400] text-[2rem]"
+            placeholder="johndoe@gmail.com"
+          />
+          <button className="flex w-[20.625rem] h-[5rem] rounded-r-[3.438rem] font-[600] font-sans text-[2rem] leading-[2.724rem] bg-white justify-center items-center">
+            Subscribe Now
+          </button>
         </div>
       </div>
     </div>
