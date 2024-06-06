bba2f3f406c2f9000b8509772900408ff9b16016
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-06 23:26:25 +0100

    refactor: Update TestimonialSection component layout and styles

--- src/components/LandingPage/TestimonialSection/TestimonialSection.js
+++ src/components/LandingPage/TestimonialSection/TestimonialSection.js
@@ -1,56 +1,53 @@
 import React from "react";
-import "./TestimonialSection.css";
 
 const TestimonialSection = () => {
   return (
-    <div className="t-s-row">
-      <div className="t-s-bg">
-        <div className="t-s-text-1-container">
-          <div className="t-s-text-1">
-            <p>Openxp Testimonials</p>
+    <div className="flex w-full h-[59.75rem] justify-center items-center">
+      <div className="flex flex-col w-[90rem] h-[56.875rem] justify-center items-center gap-[4rem]">
+        <div className="flex flex-col w-[62.875rem] h-[10rem] items-center justify-between">
+          <div className="font-[700] text-[2.5rem] leading-[3.413rem] font-sans">
+            Openxp Testimonials{" "}
           </div>
-          <div className="t-s-text-2">
-            <p>
-              Join us today and become a Straigh A student, below are Some of
-              our outstanding students who aced their exams with ease
-            </p>
+          <div className="font-[500] text-[2rem] leading-[2.724rem] text-center font-sans">
+            Join Us Today and beocome a straight A student, below are some of
+            our outstanding students, who aced their exams with ease
           </div>
         </div>
-        <div className="t-s-text-3">
-          <div className="t-s-container-left-1">
-            <div className="t-s-icon-control">
-              <img src={require("../../../icons/Mask-group.png")} />
+        <div className="flex gap-4">
+          <div
+            className="flex flex-col w-[37.438rem] h-[31.125rem] p-5 gap-3"
+            style={{ border: "1px solid #2D9CDB" }}
+          >
+            <img
+              src={require("../../../icons/Mask-group.png")}
+              className="w-[6.5rem] h-[6.5rem]"
+            />
+            <div className="font-[600] text-[1.25rem] leading-[1.703rem] font-sans">
+              Joining Openxp was a game-changer for my academic journey. Their
+              resources and support network helped me tackle my toughest
+              subjects with confidence. Thanks to their comprehensive tools and
+              expert guidance, I aced my exams and secured a scholarship to my
+              dream university. Openxp truly equips you with everything you need
+              to succeed.
             </div>
-            <div className="t-s-font-control-1">
-              <p>
-                Openxp has Played a vital role in me becoming a model student,
-                and true their help I have been able pass my final exams in
-                order for me to study internationally.Growing as up from a
-                humble background, I understand how much it means to put your
-                all, and openxp gives you a very big community to support and
-                learn.
-              </p>
-            </div>
-            <div className="t-s-name-1">
-              <p>Ariana Baker</p>
+            <div className="font-[700] text-[1.5rem] text-[#2D9CDB] font-sans">
+              Ariana Baker
             </div>
           </div>
-          <div className="t-s-container-lef-2">
-            <div className="t-s-icon-control">
-              <img src={require("../../../icons/Mask-group.png")} />
+          <div className="flex flex-col w-[37.438rem] h-[31.125rem] p-5 gap-3 shadow-custom2">
+            <img
+              src={require("../../../icons/Mask-group.png")}
+              className="w-[6.5rem] h-[6.5rem]"
+            />
+            <div className="font-[600] text-[1.25rem] leading-[1.703rem] font-sans">
+              Openxp has Played a vital role in me becoming a model student, and
+              true their help I have been able pass my final exams in order for
+              me to study internationally.Growing as up from a humble
+              background, I understand how much it means to put your all, and
+              openxp gives you a very big community to support and learn.
             </div>
-            <div className="t-s-font-control-2">
-              <p>
-                Openxp has Played a vital role in me becoming a model student,
-                and true their help I have been able pass my final exams in
-                order for me to study internationally.Growing as up from a
-                humble background, I understand how much it means to put your
-                all, and openxp gives you a very big community to support and
-                learn.
-              </p>
-            </div>
-            <div className="t-s-name-1">
-              <p>Ariana Baker</p>
+            <div className="font-[700] text-[1.5rem] text-[#2D9CDB] font-sans">
+              - Samuel Johnson
             </div>
           </div>
         </div>
