aefb13eb9a6ff7885128feee50ca1db91774e2b9
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-06 23:24:29 +0100

    refactor: Update Overlay component layout and remove unused CSS styles

--- src/components/LandingPage/OverLay/Overlay.js
+++ src/components/LandingPage/OverLay/Overlay.js
@@ -1,19 +1,16 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./OverLay.css";
 
 const OverLay = () => {
   return (
-    <div className="overlay-image">
-      <div className="hero-bg-color"></div>
+    <div className="">
       <div className="row">
-        <div className="image-container ">
-          <img
-            style={{ margin: " 0 auto" }}
+        <div className="absolute">
+          {/* <img
             src={require("./openxp.png")}
             alt="Background overlay"
             className="hero-image"
-          />
+          /> */}
         </div>
       </div>
     </div>
