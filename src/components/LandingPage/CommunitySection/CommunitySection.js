5c295ed6578d976e15854a078523f33985e70695
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-06 23:22:23 +0100

    refactor: Update CommunitySection component layout and styles

--- src/components/LandingPage/CommunitySection/CommunitySection.js
+++ src/components/LandingPage/CommunitySection/CommunitySection.js
@@ -1,77 +1,62 @@
 import React from "react";
-import "./CommunitySection.css";
+import { ReactComponent as Students } from "../../../svgs/study-group-african-people.svg";
+import { ArrowLongRightIcon } from "@heroicons/react/24/solid";
 
 const CommunitySection = () => {
   return (
-    <div className="c-s-row">
-      <div className="c-s-bg">
-        <div className="c-s-bg-container">
-          <div className="c-s-left-side">
-            <div className="c-s-left-side-1">
-              <p>
-                Beyond Exam Preparations: A Gateway to Guaranteed Exam Success
-              </p>
+    <div className="flex w-full h-[42.75rem] justify-center ">
+      <div className="flex flex-row w-[90rem] h-[42.75rem] items-center">
+        <div className="flex flex-col w-1/2 h-[33.25rem] items-center gap-5 justify-center">
+          <div className="font-[700] text-[1.75rem] leading-[2.383rem] w-[35.125rem] h-[4.75rem]">
+            Beyond Exam Preparations: A Gateway to Guaranteed Exam Success{" "}
+          </div>
+          <div className="flex w-[34.375rem] h-[12.563rem] justify-center items-center gap-[1rem]">
+            <img
+              src={require("../../../icons/teamwork.png")}
+              className="w-[1.438rem] h-[1.438rem] -mt-[7.2rem]"
+            />
+            <div className="flex flex-col w-[26.875rem] h-[8.813rem] gap-[0.5rem]">
+              <div className="font-[700] text-[1.25rem]">
+                Global Exam Preparation Community
+              </div>
+              <div className="font-[500] font-sans text-[0.938rem] leading-[1.277rem] h-[3.75rem]">
+                Join a Top tutors and Students all over the world, and learn
+                together in a beautiful Virtual community, tailored to allow you
+                thrive
+              </div>
+              <div className="flex flex-row items-center text-[#2D9CDB] gap-2">
+                <div className="font-[400] text-[1rem]">Read More</div>
+                <ArrowLongRightIcon className="w-[2rem]" />
+              </div>
             </div>
-            <div className="c-s-left-side-2">
-              <div className="c-s-drop-shadown">
-                <div className="c-c-left-side-2-internal-1">
-                  <div>
-                    <div>
-                      <img src={require("../../../icons/teamwork.png")} />
-                    </div>
-                  </div>
-                  <div className="c-s-left-internal-1-font-control-1">
-                    <p>Global Exam Preparation Community</p>
-                  </div>
-                </div>
-                <div className="c-s-left-side-2-internal-2">
-                  <p>
-                    Join a Top tutors and Students all over the world, and learn
-                    together in a beautiful Virtual community, tailored to allow
-                    you thrive
-                  </p>
-                </div>
-                <div className="c-s-left-side-2-internal-3">
-                  <div className="c-s-left-side-2-internal-3-text-1">
-                    <div>
-                      <p>Read More</p>
-                    </div>
-                    <div className="">
-                      <img
-                        className="c-s-arrow-icon"
-                        src={require("../../../icons/Arrow.png")}
-                      />
-                    </div>
-                  </div>
+          </div>
+          <div className="flex flex-col w-[35.125rem] gap-4">
+            <div className="flex h-[3rem] shadow-custom">
+              <div className="flex gap-3">
+                <img
+                  src={require("../../../icons/best-practice.png")}
+                  className="h-[1.625rem] w-[1.625rem]"
+                />
+                <div className="w-[25rem] h-[1.625rem]">
+                  Practical Tools to allow you ace your exams
                 </div>
               </div>
-              <div className="c-s-left-side-3">
-                <div className="c-s-left-side-internal-3-container">
-                  <div>
-                    <img src={require("../../../icons/best-practice.png")} />
-                  </div>
-                  <div className="c-s-side-internal-3-text">
-                    <p>Practical Tools to allow you ace your exams</p>
-                    <div className="c-s-line"></div>
-                  </div>
-                </div>
-                <div className="c-s-left-side-internal-3-container">
-                  <div>
-                    <img src={require("../../../icons/best-practice.png")} />
-                  </div>
-                  <div className="c-s-side-internal-3-text">
-                    <p>Practical Tools to allow you ace your exams</p>
-                    <div className="c-s-line"></div>
-                  </div>
+            </div>
+            <div className="flex h-[3rem] shadow-custom">
+              <div className="flex gap-3">
+                <img
+                  src={require("../../../icons/best-practice.png")}
+                  className="h-[1.625rem] w-[1.625rem]"
+                />
+                <div className="w-[25rem] h-[1.625rem]">
+                  Practical Tools to allow you ace your exams
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="c-s-icon-container">
-          <div className="c-s-icon">
-            <img src={require("../../../icons/Frame.png")} />
-          </div>
+        <div className="flex w-1/2 h-[33.25rem] justify-center">
+          <Students className="w-[37.875rem] h-[33.25rem]" />
         </div>
       </div>
     </div>
