97a7719b8342dcbad190398a4a3751ce6e1b98b3
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-06 23:25:49 +0100

    refactor: Update QuoteSection component layout and styles

--- src/components/LandingPage/QuoteSection/QuoteSection.js
+++ src/components/LandingPage/QuoteSection/QuoteSection.js
@@ -1,18 +1,15 @@
 import React from "react";
-import "./QuoteSection.css";
 
 const QuoteSection = () => {
   return (
-    <div className="my-quote-section">
-      <div className="my-quote row">
-        <div className="q-s-container-1">
-          <p className="q-s-font-control-1">
-            "Education is the most powerful weapon which you can use to changes
-            the world."
-          </p>
+    <div className="flex absolute left-1/2 transform -translate-x-1/2 bg-white w-[78.625rem] h-[17.5rem] top-[115.75rem] justify-center items-center shadow-custom2">
+      <div className="flex flex-col w-[65rem] h-[9.063rem] gap-[0.55rem]">
+        <div className="font-[700] text-[2rem] leading-[2.724rem] text-center font-sans">
+          "Education is the most powerful weapon which you can use to change the
+          world."
         </div>
-        <div className="q-s-container-2">
-          <p className="q-s-font-control-2">Nelson Mandela</p>
+        <div className="font-[400] text-[2rem] leading-[2.724rem] font-sans text-center">
+          Nelson Mandela
         </div>
       </div>
     </div>
