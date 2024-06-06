21b5bc2a2c61b7c3f55f48504b99ee38b21a3b89
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-06 23:22:44 +0100

    refactor: Update CourseCard component layout and styles

--- src/components/LandingPage/CourseCard/CourseCard.js
+++ src/components/LandingPage/CourseCard/CourseCard.js
@@ -1,76 +1,50 @@
 import React from "react";
-import "./CourseCard.css";
 
 const CourseCard = () => {
   return (
-    <div className="c-c-row">
-      <div className="c-c-bg">
-        <div className="c-c-list">
-          <div id="c-c-first-text-container">
-            <div
-              className="c-c-items-container c-c-font-control-1"
-              id="c-c-first-text"
-            >
-              <p>
-                Gear Towards Getting Your Desired Result for Scholaship Exams,
-                Internationally and Locally
-              </p>
-            </div>
+    <div className="flex flex-col h-[58.313rem] justify-center items-center z-10 -mt-[9.5rem] gap-[4rem]">
+      <div className="font-[700] text-[2.25rem] leading-[3.064rem] text-center text-white w-[63.125rem] h-[6.125rem]">
+        Gear Towards Getting Your desired Result for <br /> Scholarship Exams
+        Internationally and Locally.
+      </div>
+      <div className="flex  gap-[2rem]">
+        <div className="flex flex-col bg-white w-[23rem] h-[16.5rem] justify-center items-center rounded">
+          <img
+            src={require("../../../icons/technology.png")}
+            className="w-[5.313rem] h-[5.313rem]"
+          />
+          <div className="font-[700] text-[2rem] leading-[2.724rem] font-sans w-[13.063rem] text-center">
+            Science & Engineering
           </div>
-          <div className="c-c-icon-text-bg">
-            <div className="c-c-icon-text">
-              <div
-                className="c-c-items-container c-c-color-white"
-                id="c-c-individual"
-              >
-                <img
-                  src={require("../../../icons/technology.png")}
-                  className="c-c-container-icon"
-                />
-                <div className="c-c-internal-text-container c-c-font-control-2">
-                  <p>Science & Engineering</p>
-                </div>
-              </div>
-              <div
-                className="c-c-items-container c-c-color-blue"
-                id="c-c-individual"
-              >
-                <img
-                  src={require("../../../icons/paint-board-and-brush.png")}
-                  className="c-c-container-icon"
-                />
-                <div className="c-c-internal-text-container c-c-font-control-2 text-white">
-                  <p>Creative Arts and Craft</p>
-                </div>
-              </div>
-              <div
-                className="c-c-items-container c-c-color-white"
-                id="c-c-individual"
-              >
-                <img
-                  src={require("../../../icons/briefcase.png")}
-                  className="c-c-container-icon"
-                />
-                <div className="c-c-internal-text-container c-c-font-control-2">
-                  <p>Business & Accounting</p>
-                </div>
-              </div>
-            </div>
+        </div>
+        <div className="flex flex-col bg-[#2D9CDB] w-[23rem] h-[16.5rem] justify-center items-center rounded-lg">
+          <img
+            src={require("../../../icons/paint-board-and-brush.png")}
+            className="w-[5.313rem] h-[5.313rem]"
+          />
+          <div className="font-[700] text-[2rem] leading-[2.724rem] font-sans w-[13.063rem] text-center">
+            Creative Art & Craft
           </div>
-          <div
-            className="c-c-items-container"
-            id="c-c-text-container-2 c-c-font-control-3"
-          >
-            <p className="c-c-font-control-4">
-              Our curriculum is geared towards all kinds of professional courses
-              accross the world, our aim is solely creating an effective method
-              students can easily ace their exams without any hassle, through
-              our intense examination simulator and practices
-            </p>
+        </div>
+        <div className="flex flex-col bg-white w-[23rem] h-[16.5rem] justify-center items-center rounded">
+          <img
+            src={require("../../../icons/briefcase.png")}
+            className="w-[5.313rem] h-[5.313rem]"
+          />
+          <div className="font-[700] text-[2rem] leading-[2.724rem] font-sans w-[13.063rem] text-center">
+            Business & Accounting
           </div>
-          <div className="c-c-items-container more-button">More</div>
         </div>
       </div>
+      <div className="font-[500] text-[1.5rem] w-[70.375rem] h-[6.188rem] text-center leading-[2.043rem] text-white font-sans">
+        our curriculum is geared towards all kinds of professional courses
+        across the world, our aim is solely creating an effective and efficient
+        method for students to pass exams without hassle, through our intense
+        level of online preparation
+      </div>
+      <button className="w-[18.875rem] h-[4.375rem] bg-white text-[1.5rem] font-sans font-[700] rounded-lg">
+        More
+      </button>
     </div>
   );
 };
