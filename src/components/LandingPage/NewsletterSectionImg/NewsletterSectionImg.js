047ad1a95a22cef65bb8cb6df39b31702e2dcc32
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-06 23:24:16 +0100

    refactor: Update NewsletterSectionImg component with new CSS class for image container

--- src/components/LandingPage/NewsletterSectionImg/NewsletterSectionImg.js
+++ src/components/LandingPage/NewsletterSectionImg/NewsletterSectionImg.js
@@ -3,7 +3,7 @@ import "./NewsletterSectionImg.css";
 
 const NewsletterSectionImg = () => {
   return (
-    <div className="n-s-img-container">
+    <div className="n-s-img-container -z-20">
       <img className="n-s-bg-img" src={require("./Asset.png")} />
     </div>
   );
