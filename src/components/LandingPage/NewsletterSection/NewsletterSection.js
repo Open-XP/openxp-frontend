f394ff5ea1b9035a4df81e5330ca9c751a26df26
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-07 14:59:47 +0100

    refactor: Update NewsletterSection input styles and subscribe button border

--- src/components/LandingPage/NewsletterSection/NewsletterSection.js
+++ src/components/LandingPage/NewsletterSection/NewsletterSection.js
@@ -5,7 +5,7 @@ const NewsletterSection = () => {
     <div className="flex w-full h-[36.875rem] justify-center z-20">
       <div className="flex flex-col w-[90rem] h-[36.875rem] justify-center items-center gap-[4rem]">
         <div className="w-[64.813rem] h-[11.125rem]">
-          <div className="font-[700] text-[2.5rem] leading-[3.404rem] text-center text-[#281266] font-sans">
+          <div className="font-[700] text-[2.5rem] leading-[3.404rem] text-center text-purple-primary font-sans">
             Subscribe To Our Newsletter
           </div>
           <div className="font-[500] text-[2rem] leading-[2.724rem] text-center">
@@ -15,7 +15,7 @@ const NewsletterSection = () => {
         </div>
         <div className="flex">
           <input
-            className="w-[50.813rem] h-[5rem] rounded-l-[3.438rem] px-[5rem] placeholder-white bg-[#281266] border-none font-sans font-[400] text-[2rem] text-white"
+            className="w-[50.813rem] h-[5rem] rounded-l-[3.438rem] px-[5rem] placeholder-white bg-purple-primary border-none font-sans font-[400] text-[2rem] text-white"
             placeholder="johndoe@gmail.com"
           />
           <button className="flex w-[20.625rem] h-[5rem] rounded-r-[3.438rem] font-[600] font-sans text-[2rem] leading-[2.724rem] bg-white border-1 border-custom-border justify-center items-center">
