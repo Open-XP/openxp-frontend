1cfb9558938d4cba09d418a42bb2b1abcf31329d
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-08 01:32:00 +0100

    refactor: Update SecondSection component with responsive layout and improved image handling

--- src/components/LandingPage/SecondSection/SecondSection.js
+++ src/components/LandingPage/SecondSection/SecondSection.js
@@ -13,8 +13,8 @@ import { ReactComponent as NecoLogo } from "../../../svgs/neco-small.svg";
 const SecondSection = () => {
   return (
     <div className="flex w-full justify-center">
-      <div className="flex flex-row w-[90rem] h-[64.313rem] mt-[5rem] gap-[3rem]">
-        <div className="w-[47%] h-[33.938rem] ">
+      <div className="flex flex-col items-center 2xl:flex-row w-[90rem] h-[80rem] 2xl:h-[64.313rem] mt-[5rem] gap-[3rem] justify-center">
+        <div className="w-[45%] 2xl:w-[47%] 2xl:h-[33.938rem] 2xl:mt-0 -mt-[10rem]">
           <div className="flex flex-col gap-[1.938rem]">
             <div className="flex w-[23.625rem] gap-[0.75rem] items-center">
               <img
@@ -75,27 +75,27 @@ const SecondSection = () => {
             </div>
           </div>
         </div>
-        <div className="flex w-[53%] h-[33.938rem] ">
-          <div className="w-[38.25rem] h-[33.938rem] shadow-custom2">
-            <div className="flex h-[8.438rem] w-[38.813rem] items-center justify-center gap-[0.938rem] shadow-custom">
+        <div className="flex w-[45%] 2xl:w-[53%] h-[33.938rem] ">
+          <div className="w-full 2xl:w-[38.25rem] h-[33.938rem] shadow-custom2">
+            <div className="flex h-[8.438rem] w-full 2xl:w-[38.813rem] items-center justify-center gap-[0.938rem] shadow-custom">
               <WaecLogo className="w-[3.938rem] h-[3.938rem]" />
               <div className="font-[500] text-[1.25rem] leading-[1.703rem] font-sans box-s">
                 WAEC Examination Question and Materials
               </div>
             </div>
-            <div className="flex h-[8.438rem] w-[38.813rem] items-center justify-center gap-[0.938rem] shadow-custom">
+            <div className="flex h-[8.438rem] w-full 2xl:w-[38.813rem] items-center justify-center gap-[0.938rem] shadow-custom">
               <JambLogo className="w-[3.938rem] h-[3.938rem]" />
               <div className="font-[500] text-[1.25rem] leading-[1.703rem] font-sans">
                 JAMB Examination Question and Materials
               </div>
             </div>
-            <div className="flex h-[8.438rem] w-[38.813rem] items-center justify-center gap-[0.938rem] shadow-custom">
+            <div className="flex h-[8.438rem] w-full 2xl:w-[38.813rem] items-center justify-center gap-[0.938rem] shadow-custom">
               <NecoLogo className="w-[3.938rem] h-[3.938rem]" />
               <div className="font-[500] text-[1.25rem] leading-[1.703rem] font-sans">
                 NECO Examination Question and Materials
               </div>
             </div>
-            <div className="flex h-[8.438rem] w-[38.813rem] items-center justify-center gap-[0.938rem] shadow-custom">
+            <div className="flex h-[8.438rem] w-full 2xl:w-[38.813rem] items-center justify-center gap-[0.938rem] shadow-custom">
               <NecoLogo className="w-[3.938rem] h-[3.938rem]" />
               <div className="font-[500] text-[1.25rem] leading-[1.703rem] font-sans">
                 NCEE Examination Question and Materials
