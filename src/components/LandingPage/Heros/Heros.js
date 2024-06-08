38ab06dd2b22262265a80f50cb7eafa47ef3114a
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-08 01:13:45 +0100

    refactor: Update Heros component with responsive layout and improved image handling

--- src/components/LandingPage/Heros/Heros.js
+++ src/components/LandingPage/Heros/Heros.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+/**
+ * Renders the Heros component.
+ * The Heros component displays a hero section with a background overlay image,
+ * a title, description, and buttons for taking a test or studying.
+ * It also includes a form for users to enter their email address and password
+ * to start learning for free.
+ *
+ * @returns {JSX.Element} The rendered Heros component.
+ */
 const Heros = () => {
   return (
     <div className="relative flex w-full justify-center">
-      <div className="absolute w-full h-[55rem] bg-purple-primary -z-10"></div>
+      <div className="absolute w-full h-[80rem] 2xl:h-[55rem] bg-purple-primary -z-10"></div>
       <img
         src={require("../../../icons/overlay.png")}
         alt="Background overlay"
-        className="absolute w-[50%] h-[50rem] object-cover -z-10 mix-blend-multiply"
+        className="absolute w-[90rem] h-[50rem] object-cover -z-10 mix-blend-multiply"
       />
-      <div className="flex w-[90rem] h-[50rem] z-10 mt-[5rem]">
-        <div className="flex w-1/2 justify-end items-center">
+      <div className="flex flex-col mt-[10rem] gap-[4rem] 2xl:flex-row w-[90rem] 2xl:h-[50rem] z-10 2xl:mt-[5rem] items-center">
+        <div className="flex w-1/2 justify-center 2xl:justify-end items-center">
           <div className="flex flex-col w-[40.5rem] h-[18.9rem] justify-between">
             <h1 className="font-[700] text-[2.5rem] leading-[3.4rem] font-sans text-white">
               Prepare for Your University Exams, and Scholarship Exams.
@@ -33,8 +42,8 @@ const Heros = () => {
           </div>
         </div>
         <div className="flex w-1/2 justify-center items-center">
-          <div className="flex flex-col w-[31.375rem] h-[38.9rem] items-center justify-center gap-[1.25rem] border rounded-[0.75rem] bg-white">
-            <div className="flex flex-col w-[22.375rem] gap-[1.25rem]">
+          <div className="flex flex-col w-[90%] 2xl:w-[31.375rem] h-[44rem] 2xl:h-[38.9rem] items-center justify-center 2xl:gap-[1.25rem] gap-[2rem] border rounded-[0.75rem] bg-white">
+            <div className="flex flex-col w-[80%] 2xl:w-[22.375rem] gap-[1.25rem]">
               <h1 className="font-[700] leading-[2.213rem] text-[1.625rem] font-sans">
                 Start Preparing For Exams
               </h1>
@@ -42,30 +51,32 @@ const Heros = () => {
                 Email Address<span className="text-red-700">*</span>
               </div>
             </div>
-            <div className="flex flex-col w-[22.375rem] gap-[1.813rem]">
+            <div className="flex flex-col w-[80%] 2xl:w-[22.375rem] gap-[1.813rem]">
               <input
                 type="text"
                 autocomplete="email"
                 placeholder="Enter your email address"
-                className="w-[22.375rem] h-[3.375rem] border-none bg-[#E9F0FA] rounded-[0.563rem] pl-[1.25rem] text-[0.875rem] font-sans"
+                className="w-full h-[3.375rem] border-none bg-[#E9F0FA] rounded-[0.563rem] pl-[1.25rem] text-[0.875rem] font-sans"
               />
               <input
                 type="password"
                 autocomplete="password"
                 placeholder="Enter your password"
-                className="w-[22.375rem] h-[3.75rem] border-none bg-[#E9F0FA] rounded-[0.563rem] pl-[1.25rem] text-[0.875rem] font-sans"
+                className="w-full h-[3.75rem] border-none bg-[#E9F0FA] rounded-[0.563rem] pl-[1.25rem] text-[0.875rem] font-sans"
               />
-              <button className="w-[22.375rem] h-[3.75rem] border rounded-[0.563rem] font-sans font-[700] text-[1.25rem] bg-[#2D9CDB] text-white">
+              <button className="w-full h-[3.75rem] border rounded-[0.563rem] font-sans font-[700] text-[1.25rem] bg-[#2D9CDB] text-white">
                 Start Learning For Free
               </button>
             </div>
-            <div className="flex w-[22.375rem] justify-center items-center gap-[0.563rem]">
-              <div className="w-[8.1rem] h-[1px] bg-[#3C3C3C]"></div>
-              <div className="w-[6rem]">or Join Using</div>
-              <div className="w-[8.1rem] h-[1px] bg-[#3C3C3C]"></div>
+            <div className="flex w-[80%] 2xl:w-[22.375rem] justify-center items-center gap-[0.563rem]">
+              <div className="w-[35%] 2xl:w-[8.1rem] h-[1px] bg-[#3C3C3C]"></div>
+              <div className="w-[20%] 2xl:w-[6rem] text-center">
+                or Join Using
+              </div>
+              <div className="w-[35%] 2xl:w-[8.1rem] h-[1px] bg-[#3C3C3C]"></div>
             </div>
-            <div className="flex flex-row justify-between w-[23.625rem]">
-              <div className="flex flex-row w-[7.5rem] h-[3.188rem] justify-center items-center gap-[0.5rem]">
+            <div className="flex flex-row justify-between w-[80%] 2xl:w-[23.625rem]">
+              <div className="flex flex-row w-[30%] 2xl:w-[7.5rem] h-[3.188rem] justify-center items-center gap-[0.5rem]">
                 <img
                   className="w-[1.688rem] h-[1.688rem]"
                   src={require("../../../icons/linkedin.png")}
@@ -73,7 +84,7 @@ const Heros = () => {
                 />
                 <div className="h-[1.688rem]">LinkedIn</div>
               </div>
-              <div className="flex flex-row w-[7.5rem] h-[3.188rem] justify-center items-center gap-[0.5rem]">
+              <div className="flex flex-row w-[30%] 2xl:w-[7.5rem] h-[3.188rem] justify-center items-center gap-[0.5rem]">
                 <img
                   className="w-[1.688rem] h-[1.688rem]"
                   src={require("../../../icons/google.png")}
@@ -81,7 +92,7 @@ const Heros = () => {
                 />
                 <div className="h-[1.688rem]">Google</div>
               </div>
-              <div className="flex flex-row w-[7.5rem] h-[3.188rem] justify-center items-center gap-[0.5rem]">
+              <div className="flex flex-row w-[30%] 2xl:w-[7.5rem] h-[3.188rem] justify-center items-center gap-[0.5rem]">
                 <img
                   className="w-[1.688rem] h-[1.688rem]"
                   src={require("../../../icons/facebook.png")}
