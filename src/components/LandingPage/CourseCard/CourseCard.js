7fea59a09e26322a21e832e54eddbcdade708414
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-08 01:12:51 +0100

    refactor: adding mobile responsiveness

--- src/components/LandingPage/CourseCard/CourseCard.js
+++ src/components/LandingPage/CourseCard/CourseCard.js
@@ -1,18 +1,24 @@
 import React from "react";
 
+/**
+ * CourseCard component displays a card with information about different courses.
+ * It includes images, titles, and descriptions for each course category.
+ *
+ * @returns {JSX.Element} The CourseCard component.
+ */
 const CourseCard = () => {
   return (
-    <div className="flex flex-col h-[58.313rem] justify-center items-center z-10 -mt-[9.5rem] gap-[4rem]">
+    <div className="flex flex-col h-[100rem] 2xl:h-[58.313rem] justify-center items-center z-10 -mt-[9.5rem] gap-[4rem]">
       <img
         src={require("../../../icons/pexels-pixabay.png")}
         alt="Newsletter Background"
-        className="absolute w-full h-[58.313rem] object-cover -z-10"
+        className="absolute w-full h-[100rem] 2xl:h-[58.313rem] object-cover -z-10"
       />
       <div className="font-[700] text-[2.25rem] leading-[3.064rem] text-center text-white w-[63.125rem] h-[6.125rem]">
         Gear Towards Getting Your desired Result for <br /> Scholarship Exams
         Internationally and Locally.
       </div>
-      <div className="flex  gap-[2rem]">
+      <div className="flex flex-col 2xl:flex-row gap-[4rem] 2xl:gap-[2rem]">
         <div className="flex flex-col bg-white w-[23rem] h-[16.5rem] justify-center items-center rounded">
           <img
             src={require("../../../icons/technology.png")}
