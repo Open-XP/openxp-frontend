d090639e0c4286dfa81bdc113fb3b729b59bc3da
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-09 01:45:54 +0100

    feat: Add logout function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LogInPage from "./Pages/Accounts/LogInPage.js";
 import SignUpPage from "./Pages/Accounts/SignUpPage.js";
 import PrivateRoute from "./Pages/DashBoard/PrivateRoute.js";
 import DashBoard from "./Pages/DashBoard/DashBoard.js";
+import LogInPage_ from "./Pages/Accounts/LogInPage_.js";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LogInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login_" element={<LogInPage_ />} />
           <Route
             path="/dashboard/*"
             element={
