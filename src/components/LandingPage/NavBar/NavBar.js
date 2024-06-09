0238f8b701950e1cd6b5c5897b7b8a1031911aa2
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-09 23:39:41 +0100

    refactor: Remove unused CSS code in NavBar.css and Heros.css, update registration endpoint URL in Auth.js, and simplify resolve.alias in webpack.config.js

--- src/components/LandingPage/NavBar/NavBar.js
+++ src/components/LandingPage/NavBar/NavBar.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import OpenxpSVG from "../../../svgs/openxp.js";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Fragment } from "react";
 import {
+  MagnifyingGlassIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/20/solid";
+import {
+  Fragment,
   Menu,
   MenuButton,
   MenuItem,
@@ -16,20 +18,89 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-/**
- * Renders the navigation bar component.
- * @returns {JSX.Element} The JSX element representing the navigation bar.
- */
+const menuDetails = [
+  {
+    label: "Courses",
+    items: [
+      "Mathematics",
+      "Physics",
+      "Chemistry",
+      "Biology",
+      "Computer Science",
+      "Agricultural Science",
+      "Economics",
+      "Accounting",
+      "Geography",
+      "History",
+      "Government",
+      "Literature",
+      "English Language",
+    ],
+  },
+  {
+    label: "Resources",
+    items: [
+      "Textbooks and eBooks",
+      "Past Examination Papers",
+      "Study Guides and Notes",
+      "Tutorial Videos",
+      "Interactive Quizzes and Flashcards",
+      "Assignment and Homework Help",
+      "Reference Materials",
+      "Career Guidance Resources",
+    ],
+  },
+];
+
+const DropdownMenu = ({ label, items }) => (
+  <Menu as="div" className="relative">
+    <MenuButton className="flex no-underline text-white font-[400] text-[1.3rem] leading-[1.4rem] gap-2">
+      {label}
+      <ChevronDownIcon
+        className="-mr-1 h-5 w-5 text-gray-400"
+        aria-hidden="true"
+      />
+    </MenuButton>
+    <Transition
+      as="div"
+      enter="transition ease-out duration-100"
+      enterFrom="transform opacity-0 scale-95"
+      enterTo="transform opacity-100 scale-100"
+      leave="transition ease-in duration-75"
+      leaveFrom="transform opacity-100 scale-100"
+      leaveTo="transform opacity-0 scale-95"
+    >
+      <MenuItems className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-56 origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div className="py-1">
+          {items.map((item) => (
+            <MenuItem key={item}>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-md font-[500]"
+                  )}
+                >
+                  {item}
+                </a>
+              )}
+            </MenuItem>
+          ))}
+        </div>
+      </MenuItems>
+    </Transition>
+  </Menu>
+);
+
 const Navbar = () => {
   return (
     <div className="flex fixed h-[6rem] w-full justify-center z-20 bg-purple-primary">
       <div className="flex w-[90rem] justify-between p-4 items-center">
-        <div className="">
-          <div className="flex items-center gap-2">
-            <OpenxpSVG className="w-10 h-10 text-white" />
-            <div className="text-3xl py-4 text-white font-[700] font-sans z-10">
-              openxp
-            </div>
+        <div className="flex items-center gap-2">
+          <OpenxpSVG className="w-10 h-10 text-white" />
+          <div className="text-3xl py-4 text-white font-[700] font-sans z-10">
+            openxp
           </div>
         </div>
         <div className="w-[23rem] h-[3.25rem] items-center flex">
@@ -37,320 +108,31 @@ const Navbar = () => {
           <input
             className="w-full h-full pl-12 rounded-3xl bg-[#FFFFFF33]/25 border-none text-white text-lg outline-none placeholder-white"
             type="text"
-            placeholder="what do you want to learn?"
+            placeholder="What do you want to learn?"
           />
         </div>
         <div className="flex w-[40rem] justify-center">
           <div className="flex flex-row w-full gap-3 justify-around items-center px-3">
-            <div className="">
-              <Menu>
-                <MenuButton className="flex no-underline text-white font-[400] text-[1.3rem] leading-[1.4rem] gap-2">
-                  Courses
-                  <ChevronDownIcon
-                    className="-mr-1 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </MenuButton>
-                <Transition
-                  as="div"
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <MenuItems className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-56 origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Edit
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                    </div>
-                  </MenuItems>
-                </Transition>
-              </Menu>
-            </div>
-            <div>
-              <Menu>
-                <MenuButton className="flex no-underline text-white font-[400] text-[1.3rem] leading-[1.4rem] gap-2">
-                  Resources
-                  <ChevronDownIcon
-                    className="-mr-1 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </MenuButton>
-                <Transition
-                  as="div"
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <MenuItems className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-56 origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Edit
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                    </div>
-                  </MenuItems>
-                </Transition>
-              </Menu>
-            </div>
-            <div>
-              <Menu>
-                <MenuButton className="flex no-underline text-white font-[400] text-[1.3rem] leading-[1.4rem] gap-2">
-                  About us
-                  <ChevronDownIcon
-                    className="-mr-1 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </MenuButton>
-                <Transition
-                  as="div"
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <MenuItems className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-56 origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Edit
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "block px-4 py-2 text-sm"
-                            )}
-                          >
-                            Delete
-                          </a>
-                        )}
-                      </MenuItem>
-                    </div>
-                  </MenuItems>
-                </Transition>
-              </Menu>
-            </div>
+            {menuDetails.map((menu) => (
+              <DropdownMenu
+                key={menu.label}
+                label={menu.label}
+                items={menu.items}
+              />
+            ))}
+            <Link
+              to="/about-us"
+              className="no-underline text-[1.3rem] leading-[1.4rem] text-white"
+            >
+              About Us
+            </Link>
             <div className="h-14 w-[1px] bg-white font-[400]"></div>
-            <div id="no-underline text-white font-[400] ">
-              <Link
-                to="/dashboard"
-                className="no-underline text-[1.3rem] leading-[1.4rem] text-white"
-              >
-                LogIn
-              </Link>
-            </div>
+            <Link
+              to="/dashboard"
+              className="no-underline text-[1.3rem] leading-[1.4rem] text-white"
+            >
+              LogIn
+            </Link>
             <div className="flex justify-center items-center w-[8rem] h-[3rem] rounded-lg border border-white">
               <Link
                 to="/signup"
