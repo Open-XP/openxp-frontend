bb36afe3deaf300a388a49511aad98b8178880e2
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-09 13:51:27 +0100

    refactor: Update App.js to fix import statements and remove unused code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Import components
 import LandingPage from "./components/LandingPage/LandingPage.js";
-import LogInPage from "./Pages/Accounts/LogInPage.js";
 import SignUpPage from "./Pages/Accounts/SignUpPage.js";
 import PrivateRoute from "./Pages/DashBoard/PrivateRoute.js";
 import DashBoard from "./Pages/DashBoard/DashBoard.js";
-import LogInPage_ from "./Pages/Accounts/LogInPage_.js";
+import LogInPage from "./Pages/Accounts/LogInPage.js";
 import "./App.css";
 
 function App() {
@@ -15,9 +14,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LogInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login_" element={<LogInPage_ />} />
+          <Route path="/login" element={<LogInPage />} />
           <Route
             path="/dashboard/*"
             element={
