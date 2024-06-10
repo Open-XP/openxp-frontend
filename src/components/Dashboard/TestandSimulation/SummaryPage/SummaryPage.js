e26a87134b68c3a0ab7b2e9cbf8ce76191a9444c
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-10 18:40:21 +0100

    refactor: Optimize SummaryPage component and remove unused code

---
+++ src/components/Dashboard/TestandSimulation/SummaryPage/SummaryPage.js
@@ -0,0 +1,14 @@
+import { React, Component } from "react";
+
+class SummaryPage extends Component {
+  render() {
+    return (
+      <div className="w-screen h-screen ">
+        <div className="w-[40%] bg-purple-primary h-full"></div>
+        <div className="w-[60%]"></div>
+      </div>
+    );
+  }
+}
+
+export default SummaryPage;
