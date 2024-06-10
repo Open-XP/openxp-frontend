f39060c0b7b219f6bf71a45688b5933bd5e95c1e
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-11 00:26:26 +0100

    refactor: Optimize routing and component structure in DashContent.js

--- src/components/Dashboard/DashContent.js
+++ src/components/Dashboard/DashContent.js
@@ -1,10 +1,50 @@
 import React from "react";
-import DashoardContent from "./MainDashHome/DashboardContent";
+import DashboardContent from "../../components/Dashboard/MainDashHome/DashboardContent";
+import SideBar from "./DashBoardSideBar/SideBar";
+import DashboardNavBar from "./DashBoardSideBar/DashboardNavBar";
+import { Routes, Route } from "react-router-dom";
+import Void from "./DashBoardSideBar/void";
+import MockExam from "../../components/Dashboard/TestandSimulation/MockExams/MockExam";
+import Evaluation from "../../components/Dashboard/TestandSimulation/EvaluationScreen/Evaluation";
+import SummaryPage from "./TestandSimulation/SummaryPage/SummaryPage";
 
 export function DashContent() {
   return (
     <div>
-      <DashoardContent />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <DashboardNavBar />
+              <div className="flex">
+                <Void />
+                <div className="flex w-full justify-center mt-[10rem]">
+                  <DashboardContent />
+                </div>
+              </div>
+              <SideBar />
+            </div>
+          }
+        />
+        <Route
+          path="/mock-exam"
+          element={
+            <div>
+              <DashboardNavBar className="-mt" />
+              <div className="flex">
+                <Void />
+                <div className="flex w-full justify-center mt-[10rem]">
+                  <MockExam />
+                </div>
+              </div>
+              <SideBar />
+            </div>
+          }
+        />
+        <Route path="/mock-exam/evaluation/:id" element={<Evaluation />} />
+        <Route path="/summary" element={<SummaryPage/>} />
+      </Routes>
     </div>
   );
 }
