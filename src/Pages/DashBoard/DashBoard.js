d15050a098bc33262ed4d7bd7b06d50d7a70d107
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-11 00:27:20 +0100

    refactor: Update DashBoard.js to use DashContent component

--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -7,6 +7,7 @@ import Void from "../../components/Dashboard/DashBoardSideBar/void";
 import MockExam from "../../components/Dashboard/TestandSimulation/MockExams/MockExam";
 import Evaluation from "../../components/Dashboard/TestandSimulation/EvaluationScreen/Evaluation";
 import SummaryPage from "../../components/Dashboard/TestandSimulation/SummaryPage/SummaryPage";
+import DashContent from "../../components/Dashboard/DashContent";
 
 const NewDashBoard = () => {
   const location = useLocation();
@@ -16,25 +17,30 @@ const NewDashBoard = () => {
     location.pathname.includes("/mock-exam/evaluation/") ||
     location.pathname.includes("dashboard/summary");
 
+  // return (
+  //   <div className="m-0">
+  //     <div>
+  //       <Routes>
+  //         <Route path="/summary" element={<SummaryPage />} />
+  //       </Routes>
+  //     </div>
+  //     {!isEvaluationRoute && <DashboardNavBar />}
+  //     <div className="flex w-screen h-screen pt-40 gap-3">
+  //       {!isEvaluationRoute && <Void />}
+  //       <div className="flex-grow flex items-center justify-center">
+  //         <Routes>
+  //           <Route path="/" element={<DashboardContent />} />
+  //           <Route path="/mock-exam" element={<MockExam />} />
+  //           <Route path="/mock-exam/evaluation/:id" element={<Evaluation />} />
+  //         </Routes>
+  //       </div>
+  //     </div>
+  //     {!isEvaluationRoute && <SideBar />}
+  //   </div>
+  // );
   return (
-    <div className="m-0">
-      <div>
-        <Routes>
-          <Route path="/summary" element={<SummaryPage />} />
-        </Routes>
-      </div>
-      {!isEvaluationRoute && <DashboardNavBar />}
-      <div className="flex w-screen h-screen pt-40 gap-3">
-        {!isEvaluationRoute && <Void />}
-        <div className="flex-grow flex items-center justify-center">
-          <Routes>
-            <Route path="/" element={<DashboardContent />} />
-            <Route path="/mock-exam" element={<MockExam />} />
-            <Route path="/mock-exam/evaluation/:id" element={<Evaluation />} />
-          </Routes>
-        </div>
-      </div>
-      {!isEvaluationRoute && <SideBar />}
+    <div>
+      <DashContent />
     </div>
   );
 };
