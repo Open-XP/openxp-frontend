fd815b7e8b39801b30dc0266de5d1b6267a86607
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-06-10 18:40:44 +0100

    refactor: Optimize routing in DashBoard.js

--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -2,30 +2,35 @@ import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import SideBar from "../../components/Dashboard/DashBoardSideBar/SideBar";
 import DashboardNavBar from "../../components/Dashboard/DashBoardSideBar/DashboardNavBar";
-import DashboardContent from "../../components/Dashboard/DashContent/DashboardContent/DashboardContent";
-import Void from "../../components/Dashboard/DashContent/DashboardContent/void";
-import MockExam from "../../components/Dashboard/MockExams/MockExam";
-import Evaluation from "../../components/Dashboard/EvaluationScreen/Evaluation";
+import DashboardContent from "../../components/Dashboard/MainDashHome/DashboardContent";
+import Void from "../../components/Dashboard/DashBoardSideBar/void";
+import MockExam from "../../components/Dashboard/TestandSimulation/MockExams/MockExam";
+import Evaluation from "../../components/Dashboard/TestandSimulation/EvaluationScreen/Evaluation";
+import SummaryPage from "../../components/Dashboard/TestandSimulation/SummaryPage/SummaryPage";
 
 const NewDashBoard = () => {
   const location = useLocation();
 
   // Check if the current path includes "evaluation"
-  const isEvaluationRoute = location.pathname.includes(
-    "/mock-exam/evaluation/"
-  );
+  const isEvaluationRoute =
+    location.pathname.includes("/mock-exam/evaluation/") ||
+    location.pathname.includes("dashboard/summary");
 
   return (
     <div className="m-0">
+      <div>
+        <Routes>
+          <Route path="/summary" element={<SummaryPage />} />
+        </Routes>
+      </div>
       {!isEvaluationRoute && <DashboardNavBar />}
-      <div className="flex w-screen h-full pt-40 gap-3">
+      <div className="flex w-screen h-screen pt-40 gap-3">
         {!isEvaluationRoute && <Void />}
         <div className="flex-grow flex items-center justify-center">
           <Routes>
             <Route path="/" element={<DashboardContent />} />
-            <Route path="mock-exam" element={<MockExam />} />
+            <Route path="/mock-exam" element={<MockExam />} />
             <Route path="/mock-exam/evaluation/:id" element={<Evaluation />} />
-            {/* Add more dashboard specific routes here */}
           </Routes>
         </div>
       </div>
