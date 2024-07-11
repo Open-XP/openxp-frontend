e3a637f024e255f3d56320a2b979ff1e97a298e5
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-07-11 22:28:37 +0100

    feat: Add password reset functionality and pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import SignUpPage from "./Pages/Accounts/SignUpPage.js";
 import PrivateRoute from "./Pages/DashBoard/PrivateRoute.js";
 import DashBoard from "./Pages/DashBoard/DashBoard.js";
 import LogInPage from "./Pages/Accounts/LogInPage.js";
+import PasswordResetPage from "./Pages/Accounts/PasswordResetPage.js";
+import ConfirmPasswordResetPage from "./Pages/Accounts/ConfirmPasswordResetPage.js";
 import "./App.css";
+import PasswordResetSent from "./Pages/PasswordReset/PasswordResetSent.js";
 
 function App() {
   return (
@@ -23,6 +26,12 @@ function App() {
                 <DashBoard />
               </PrivateRoute>
             }
+          />
+          <Route path="/password-reset" element={<PasswordResetPage />} />
+          <Route path="/password-reset-sent" element={<PasswordResetSent />} />
+          <Route
+            path="/confirm-password-reset/:uidb64/:token"
+            element={<ConfirmPasswordResetPage />}
           />
         </Routes>
       </Router>
