fef45cbd55cf901aa92b92337e8bf26f82419046
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-07-19 17:10:27 +0100

    feat: Implement CRUD functionality for schedules

--- src/components/Dashboard/SchedulePlan/ScheduleComponent/AddSchedule.js
+++ src/components/Dashboard/SchedulePlan/ScheduleComponent/AddSchedule.js
@@ -4,16 +4,107 @@ import { ArrowLongRightIcon } from "@heroicons/react/24/solid";
 import WriteIcon from "../../../../icons/write-icon.png";
 import PropTypes from "prop-types";
 import { withRouterHooks } from "../../../../withRouters/withRoutersHook";
+import { connect } from "react-redux";
+import {
+  fetchAllSchedules,
+  createSchedule,
+  updateSchedule,
+  fetchIndividualSchedule,
+} from "../../../../Actions/Schedule";
 
 class AddSchedule extends Component {
   state = {
     isDescriptionFocused: false,
     description: "",
+    subject: "",
+    exam_type: "",
+    date: "",
+    time: "",
+    id: null,
   };
 
   static propTypes = {
     navigate: PropTypes.func.isRequired,
-    location: PropTypes.func.isRequired,
+    location: PropTypes.object.isRequired,
+    schedules: PropTypes.array.isRequired,
+    schedule: PropTypes.object.isRequired,
+    fetchAllSchedules: PropTypes.func.isRequired,
+    createSchedule: PropTypes.func.isRequired,
+    updateSchedule: PropTypes.func.isRequired,
+    fetchIndividualSchedule: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    const { fetchIndividualSchedule, location } = this.props;
+    const pathParts = location.pathname.split("/");
+    const id = pathParts[pathParts.length - 1];
+    this.setState({ id });
+
+    if (location.pathname === `/dashboard/schedule-plan/edit/${id}`) {
+      fetchIndividualSchedule(id);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.schedule !== prevProps.schedule) {
+      const { subject, exam_type, date, time, description } =
+        this.props.schedule;
+      this.setState({
+        subject,
+        exam_type,
+        date,
+        time,
+        description,
+      });
+    }
+  }
+
+  handleCreateOrUpdate = (e) => {
+    e.preventDefault();
+    const { location, updateSchedule, createSchedule } = this.props;
+    const { subject, exam_type, date, time, description } = this.state;
+    const pathParts = location.pathname.split("/");
+    const id = pathParts[pathParts.length - 1];
+    console.log({ id, subject, exam_type, date, time, description });
+
+    // Validate inputs
+    if (!subject || typeof subject !== "string") {
+      alert("Subject is not valid");
+      return;
+    }
+    if (!exam_type || typeof exam_type !== "string") {
+      alert("Exam Type is not valid");
+      return;
+    }
+    if (!date || typeof date !== "string") {
+      alert("Date is required");
+      return;
+    }
+    if (!time || typeof time !== "string") {
+      alert("Time is required");
+      return;
+    }
+
+    const scheduleData = {
+      subject: String(subject),
+      exam_type: String(exam_type),
+      date: String(date),
+      time: String(time),
+      description: String(description),
+    };
+
+    console.log("Sending schedule data:", scheduleData);
+
+    if (location.pathname === `/dashboard/schedule-plan/edit/${id}`) {
+      updateSchedule(id, scheduleData);
+    } else if (location.pathname === `/dashboard/schedule-plan/create`) {
+      createSchedule(scheduleData);
+    }
+  };
+
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
   };
 
   ReturnToSchedule = () => {
@@ -25,7 +116,7 @@ class AddSchedule extends Component {
   };
 
   handleDescriptionBlur = () => {
-    if (this.state.description.trim() === "") {
+    if ((this.state.description || "").trim() === "") {
       this.setState({ isDescriptionFocused: false });
     }
   };
@@ -35,16 +126,25 @@ class AddSchedule extends Component {
   };
 
   render() {
-    const { isDescriptionFocused, description } = this.state;
+    const {
+      isDescriptionFocused,
+      description,
+      subject,
+      exam_type,
+      date,
+      time,
+    } = this.state;
 
     const { location } = this.props;
     const HeaderText =
-      location.pathname === "/dashboard/schedule-plan/edit"
-        ? "Edit Schedule"
-        : "Add Schedule";
+      location.pathname === `/dashboard/schedule-plan/create`
+        ? "Add Schedule"
+        : "Edit Schedule";
 
     const SaveOrUpdate =
-      location.pathname === "/dashboard/schedule-plan/edit" ? "Update" : "Save";
+      location.pathname === "/dashboard/schedule-plan/create"
+        ? "Save"
+        : "Update";
 
     return (
       <div className="flex flex-col w-[95%] font-inter my-4 gap-[3rem] p-8">
@@ -59,11 +159,14 @@ class AddSchedule extends Component {
           </div>
         </div>
         <div>
-          <div className="font-[400] text-[1.5rem]">Event Title</div>
+          <div className="font-[400] text-[1.5rem]">Subject</div>
           <input
             className="w-full h-[4rem] border-2 rounded-[0.875rem] text-[1.5rem]"
             type="text"
             placeholder=""
+            value={subject}
+            onChange={this.onChange}
+            name="subject"
           />
         </div>
         <div>
@@ -72,6 +175,9 @@ class AddSchedule extends Component {
             className="w-full h-[4.5rem] text-[1.5rem] rounded-[0.875rem]"
             type="text"
             placeholder=""
+            value={exam_type}
+            onChange={this.onChange}
+            name="exam_type"
           />
         </div>
         <div className="flex gap-[2rem] items-center">
@@ -80,8 +186,9 @@ class AddSchedule extends Component {
             <input
               className="w-[15.125rem] h-[3.813rem] text-[1.5rem] border-2 rounded-[3.813rem]"
               type="date"
-              onFocus={this.handleDescriptionFocus}
-              onBlur={this.handleDescriptionBlur}
+              onChange={this.onChange}
+              value={date}
+              name="date"
             />
           </div>
           <div className="flex w-[4rem] h-[2rem] items-center mt-[1.8rem]">
@@ -92,11 +199,14 @@ class AddSchedule extends Component {
             <input
               className="w-[12.125rem] h-[3.813rem] text-[1.5rem] border-2 rounded-[3.813rem]"
               type="time"
+              onChange={this.onChange}
+              value={time}
+              name="time"
             />
           </div>
         </div>
         <div className="flex relative">
-          {!isDescriptionFocused && description.trim() === "" && (
+          {!isDescriptionFocused && (description || "").trim() === "" && (
             <div className="flex items-center absolute p-[1rem] gap-2 -z-10">
               <img
                 className="w-[2rem] h-[2rem]"
@@ -106,17 +216,22 @@ class AddSchedule extends Component {
               <div className="font-[400] text-[1.5rem]">Add Description</div>
             </div>
           )}
-          <input
-            className="w-full h-[10rem] border-2 rounded-[0.875rem]"
+          <textarea
+            className="w-full h-[10rem] border-2 rounded-[0.875rem] p-[1rem]"
             placeholder=""
             onFocus={this.handleDescriptionFocus}
             onBlur={this.handleDescriptionBlur}
             onChange={this.handleDescriptionChange}
             value={description}
+            name="description"
           />
         </div>
         <div className="flex justify-end">
-          <button className="flex justify-center items-center w-[13.375rem] h-[3.438rem] bg-purple-primary rounded-[0.438rem] text-white font-[600] text-lg 2xl:text-[2.25rem] leading-[2.043rem]">
+          <button
+            type="submit"
+            className="flex justify-center items-center w-[13.375rem] h-[3.438rem] bg-purple-primary rounded-[0.438rem] text-white font-[600] text-lg 2xl:text-[2.25rem] leading-[2.043rem]"
+            onClick={this.handleCreateOrUpdate}
+          >
             <div>{SaveOrUpdate}</div>
           </button>
         </div>
@@ -125,4 +240,18 @@ class AddSchedule extends Component {
   }
 }
 
-export default withRouterHooks(AddSchedule);
+const mapStateToProps = (state) => ({
+  schedules: state.scheduleexam.schedules,
+  schedule: state.scheduleexam.schedule,
+});
+
+const mapDispatchToProps = {
+  createSchedule,
+  fetchAllSchedules,
+  updateSchedule,
+  fetchIndividualSchedule,
+};
+
+export default withRouterHooks(
+  connect(mapStateToProps, mapDispatchToProps)(AddSchedule)
+);
