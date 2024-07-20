a285b0d8a278978f54494d09ba29ba1df82f7109
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-07-20 21:16:01 +0100

    chore: Add deployment script for frontend

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Starting SSH agent"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/school
+
+echo "Switching to branch master"
+git checkout master
+
+echo "Building frontend"
+npm run build
+
+echo "Deploying frontend"
+scp -o StrictHostKeyChecking=no -i ~/.ssh/school -r build/* ubuntu@54.173.133.193:/var/www/54.173.133.193
+
+echo "Frontend deployed"
