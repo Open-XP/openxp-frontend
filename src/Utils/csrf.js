1d8e1667771701c2a733b2d39c72ef72f44b81f5
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-07-30 13:37:48 +0100

    chore: Refactor CSRF token retrieval to use getCookie function

--- src/Utils/csrf.js
+++ src/Utils/csrf.js
@@ -1,6 +1,15 @@
-// src/utils/csrf.js
-import Cookies from 'js-cookie';
-
-export function getCsrfToken() {
-  return Cookies.get('csrftoken');
+// utils/csrf.js
+export function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
 }
