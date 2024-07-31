c4fafdc25573ef34f109aa71b072bb7adf1fdcf7
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-07-31 11:23:48 +0100

    chore: Update AI action to explain answer with prompt

--- src/Actions/AI.js
+++ src/Actions/AI.js
@@ -1,19 +1,24 @@
 import axios from "../Utils/axios";
+import { baseurl } from "../Actions/Auth";
+import { tokenConfig } from "./Auth";
 import { returnErrors } from "./Messages";
 import { EXPLAIN_ANSWER_SUCCESS, EXPLAIN_ANSWER_FAIL } from "./Types";
 
-export const explainAnswer = (question) => (dispatch) => {
-  const body = JSON.stringify({ question });
-  axios
-    .post(`${baseurl}/api/ai/explain-answer/`, body, tokenConfig(getState))
-    .then((res) =>
+export const explainAnswer = (prompt) => (dispatch, getState) => {
+  const body = JSON.stringify({ prompt });
+  console.log("body", body);
+  return axios
+    .post(`${baseurl}/api/ai/explain_answers/`, body, tokenConfig(getState))
+    .then((res) => {
       dispatch({
         type: EXPLAIN_ANSWER_SUCCESS,
         payload: res.data,
-      })
-    )
+      });
+      return res.data; // Return the response data for the promise chain
+    })
     .catch((err) => {
       dispatch(returnErrors(err.response.data, err.response.status));
       dispatch({ type: EXPLAIN_ANSWER_FAIL });
+      throw err; // Throw the error for the promise chain
     });
 };
