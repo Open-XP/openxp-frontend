111c33e4d5126db5e29a8680910a1353029daec6
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-08-02 01:21:00 +0100

    chore: Add new animation styles to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,22 @@ module.exports = {
         "skyblue-secondary": "#2D9CDB",
       },
       keyframes: {
+        scaleDownCenter: {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "100%": {
+            transform: "scale(0.5)",
+          },
+        },
+        scaleUpCenter: {
+          "0%": {
+            transform: "scale(0.5)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
         slideIn: {
           "0%": { transform: "translateX(100%)" },
           "100%": { transform: "translateX(0)" },
@@ -39,10 +55,133 @@ module.exports = {
           "0%": { transform: "translateX(0)" },
           "100%": { transform: "translateX(-100%)" },
         },
+        bounceInTop: {
+          "0%": {
+            "animation-timing-function": "ease-in",
+            opacity: "0",
+            transform: "translateY(-250px)",
+          },
+          "38%": {
+            "animation-timing-function": "ease-out",
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          "55%": {
+            "animation-timing-function": "ease-in",
+            transform: "translateY(-65px)",
+          },
+          "72%": {
+            "animation-timing-function": "ease-out",
+            transform: "translateY(0)",
+          },
+          "81%": {
+            "animation-timing-function": "ease-in",
+            transform: "translateY(-28px)",
+          },
+          "90%": {
+            "animation-timing-function": "ease-out",
+            transform: "translateY(0)",
+          },
+          "95%": {
+            "animation-timing-function": "ease-in",
+            transform: "translateY(-8px)",
+          },
+          "100%": {
+            "animation-timing-function": "ease-out",
+            transform: "translateY(0)",
+          },
+        },
+        slideInTop: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-250px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        jelloHorizontal: {
+          "0%": {
+            transform: "scale3d(1, 1, 1)",
+          },
+          "30%": {
+            transform: "scale3d(1.25, 0.75, 1)",
+          },
+          "40%": {
+            transform: "scale3d(0.75, 1.25, 1)",
+          },
+          "50%": {
+            transform: "scale3d(1.15, 0.85, 1)",
+          },
+          "65%": {
+            transform: "scale3d(0.95, 1.05, 1)",
+          },
+          "75%": {
+            transform: "scale3d(1.05, 0.95, 1)",
+          },
+          "100%": {
+            transform: "scale3d(1, 1, 1)",
+          },
+        },
+        slideInButtom: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(250px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        bounceInBottom: {
+          "0%": {
+            "animation-timing-function": "ease-in",
+            opacity: "0",
+            transform: "translateY(250px)",
+          },
+          "38%": {
+            "animation-timing-function": "ease-out",
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          "55%": {
+            "animation-timing-function": "ease-in",
+            transform: "translateY(65px)",
+          },
+          "72%": {
+            "animation-timing-function": "ease-out",
+            transform: "translateY(0)",
+          },
+          "81%": {
+            "animation-timing-function": "ease-in",
+            transform: "translateY(28px)",
+          },
+          "90%": {
+            "animation-timing-function": "ease-out",
+            transform: "translateY(0)",
+          },
+          "95%": {
+            "animation-timing-function": "ease-in",
+            transform: "translateY(8px)",
+          },
+          "100%": {
+            "animation-timing-function": "ease-out",
+            transform: "translateY(0)",
+          },
+        },
       },
       animation: {
         slideIn: "slideIn 0.5s ease-in-out forwards",
         slideOut: "slideOut 0.5s ease-in-out forwards",
+        bounceInTop: "bounceInTop 1s both",
+        slideInTop: "slideInTop 1s ease-out both",
+        scaleUpCenter: "scaleUpCenter 0.5s ease-in-out forwards",
+        scaleDownCenter: "scaleDownCenter",
+        scaleDownCenter: "scaleDownCenter 0.5s ease-in-out forwards",
+        jelloHorizontal: "scaleDownCenter 0.75s ease-in-out forwards",
+        slideInButtom: "scaleDownCenter 500ms ease 0s 1 alternate none",
+        bounceInBottom: "bounceInBottom 1s both",
       },
     },
   },
