81ba98b9474f0f1ca3eeace50e209f57588e975d
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-08-04 09:15:02 +0100

    chore: Add AI chat session related action types and reducers

--- src/Reducers/AI.js
+++ src/Reducers/AI.js
@@ -1,9 +1,23 @@
-import { EXPLAIN_ANSWER_SUCCESS, EXPLAIN_ANSWER_FAIL } from "../Actions/Types";
+import {
+  EXPLAIN_ANSWER_SUCCESS,
+  EXPLAIN_ANSWER_FAIL,
+  START_CHAT_SESSION_SUCCESS,
+  START_CHAT_SESSION_FAIL,
+  FETCH_INDIVIDUAL_CHAT_SESSIONS_SUCCESS,
+  FETCH_INDIVIDUAL_CHAT_SESSIONS_FAIL,
+  FETCH_ALL_CHAT_SESSION_SUCCESS,
+  FETCH_ALL_CHAT_SESSION_FAIL,
+} from "../Actions/Types";
 
 const initialState = {
   explanation: [],
   error: {},
   explainatioLoading: true,
+  careerBuddyStarted: false,
+  loadingIndividualChatSessions: true,
+  loadingAllChatSessions: true,
+  individualChatSessions: [],
+  allChatSessions: [],
 };
 
 export default function (state = initialState, action) {
@@ -17,6 +31,38 @@ export default function (state = initialState, action) {
         explainatioLoading: false,
       };
     case EXPLAIN_ANSWER_FAIL:
+      return {
+        ...state,
+        error: payload,
+      };
+    case START_CHAT_SESSION_SUCCESS:
+      return {
+        ...state,
+        careerBuddyStarted: true,
+      };
+    case START_CHAT_SESSION_FAIL:
+      return {
+        ...state,
+        error: payload,
+      };
+    case FETCH_INDIVIDUAL_CHAT_SESSIONS_SUCCESS:
+      return {
+        ...state,
+        individualChatSessions: payload,
+        loadingIndividualChatSessions: false,
+      };
+    case FETCH_INDIVIDUAL_CHAT_SESSIONS_FAIL:
+      return {
+        ...state,
+        error: payload,
+      };
+    case FETCH_ALL_CHAT_SESSION_SUCCESS:
+      return {
+        ...state,
+        allChatSessions: payload,
+        loadingAllChatSessions: false,
+      };
+    case FETCH_ALL_CHAT_SESSION_FAIL:
       return {
         ...state,
         error: payload,
