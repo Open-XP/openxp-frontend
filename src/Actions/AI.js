fbc30057ce42abce4f41f12222d71ff9ebd6bc08
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-08-04 09:07:31 +0100

    chore: Update AI.js with new actions for chat sessions

--- src/Actions/AI.js
+++ src/Actions/AI.js
@@ -1,8 +1,17 @@
 import axios from "../Utils/axios";
-import { baseurl } from "../Actions/Auth";
+import { baseurl } from "./Auth";
 import { tokenConfig } from "./Auth";
 import { returnErrors } from "./Messages";
-import { EXPLAIN_ANSWER_SUCCESS, EXPLAIN_ANSWER_FAIL } from "./Types";
+import {
+  EXPLAIN_ANSWER_SUCCESS,
+  EXPLAIN_ANSWER_FAIL,
+  START_CHAT_SESSION_SUCCESS,
+  START_CHAT_SESSION_FAIL,
+  FETCH_INDIVIDUAL_CHAT_SESSIONS_SUCCESS,
+  FETCH_INDIVIDUAL_CHAT_SESSIONS_FAIL,
+  FETCH_ALL_CHAT_SESSION_SUCCESS,
+  FETCH_ALL_CHAT_SESSION_FAIL,
+} from "./Types";
 
 export const explainAnswer = (prompt) => (dispatch, getState) => {
   const body = JSON.stringify({ prompt });
@@ -20,5 +29,54 @@ export const explainAnswer = (prompt) => (dispatch, getState) => {
       dispatch(returnErrors(err.response.data, err.response.status));
       dispatch({ type: EXPLAIN_ANSWER_FAIL });
       throw err; // Throw the error for the promise chain
+    });
+};
+
+export const StartCareerBuddySession = () => (dispatch, getState) => {
+  const body = {};
+  return axios
+    .post(`${baseurl}/api/ai/sessions/`, body, tokenConfig(getState)) // Pass the empty body
+    .then((res) =>
+      dispatch({
+        type: START_CHAT_SESSION_SUCCESS,
+        payload: res.data,
+      })
+    )
+    .catch((err) => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({ type: START_CHAT_SESSION_FAIL });
+      throw err;
+    });
+};
+
+export const FetchIndividualChatSessions = (id) => (dispatch, getState) => {
+  return axios
+    .get(`${baseurl}/api/ai/sessions/${id}/`, tokenConfig(getState))
+    .then((res) =>
+      dispatch({
+        type: FETCH_INDIVIDUAL_CHAT_SESSIONS_SUCCESS,
+        payload: res.data,
+      })
+    )
+    .catch((err) => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({ type: FETCH_INDIVIDUAL_CHAT_SESSIONS_FAIL });
+      throw err;
+    });
+};
+
+export const FetchAllChatSessions = () => (dispatch, getState) => {
+  return axios
+    .get(`${baseurl}/api/ai/sessions/`, tokenConfig(getState))
+    .then((res) =>
+      dispatch({
+        type: FETCH_ALL_CHAT_SESSION_SUCCESS, // Correct the typo here
+        payload: res.data,
+      })
+    )
+    .catch((err) => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({ type: FETCH_ALL_CHAT_SESSION_FAIL });
+      throw err;
     });
 };
