1db40142ffe67d50c503f22cd121897f679957f5
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-08-06 23:44:49 +0100

    feat: Add ThreeDotter component for displaying a loading animation

---
+++ src/Animations/ThreeDotter/ThreeDotter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ThreeDotter.css";
+
+const ThreeDotter = () => {
+  <div class="lds-ellipsis">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>;
+};
+
+export default ThreeDotter;
