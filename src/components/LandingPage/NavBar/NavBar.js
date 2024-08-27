4d3a488e0bf558b4458a38324d7c819a4309a649
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-08-27 03:05:40 +0100

    Refactor NavBar.js to fix broken link in DropdownMenu component

--- src/components/LandingPage/NavBar/NavBar.js
+++ src/components/LandingPage/NavBar/NavBar.js
@@ -76,7 +76,7 @@ const DropdownMenu = ({ label, items }) => (
             <MenuItem key={item}>
               {({ active }) => (
                 <a
-                  href="#"
+                  href=""
                   className={classNames(
                     active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                     "block px-4 py-2 text-md font-[500]"
