a1f4d6b47b0acb1e4c360008f97a5014a5896cf6
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-08-27 02:39:25 +0100

    chore: Update deploy.sh script to build and deploy frontend application

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Start the SSH agent and add the private key
 echo "Starting SSH agent"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/school
 
+# Switch to the desired branch
 echo "Switching to branch master"
-git checkout main
+git checkout main  # Uncomment this if you need to switch branches
 
-# echo "Building App"
-# npm run build
+# Build the frontend application
+echo "Building App"
+npm run build
 
+# Deploy the frontend to the remote server
 echo "Deploying frontend"
-scp -o StrictHostKeyChecking=no -i ~/.ssh/school -r build/* ubuntu@13.49.145.101:~/build
+rsync -avz --exclude 'node_modules' --exclude 'venv' -e "ssh -i ~/.ssh/school -o StrictHostKeyChecking=no" ./build mannie@52.91.131.166:~/build
 echo "App deployed"
