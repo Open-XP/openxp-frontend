2177829c60709a8e96a71726735fd6b910feb5af
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-08-30 15:30:59 +0100

    Refactor BaseUrls.js to update the base URL for API endpoints

---
+++ src/Actions/BaseUrls.js
@@ -0,0 +1,7 @@
+// const baseurl = "https://kaput-cannon-obedient-walk-production.pipeops.app";
+// export const baseurl = "http://13.49.145.101";
+// export const baseurl = "http://127.0.0.1:8000";
+// export const baseurl = "http://54.157.163.2";
+// export const baseurl = "http://13.60.167.14";
+
+export const baseurl = "http://openxp.study";
