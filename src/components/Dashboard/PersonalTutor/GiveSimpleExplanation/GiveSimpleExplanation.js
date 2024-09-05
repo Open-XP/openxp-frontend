290a19c32000b3ea74aa8e0d991a93e798fd6e9b
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-09-05 05:25:09 +0100

    feat: Add GiveSimpleExplanation component
    Add the `GiveSimpleExplanation` component to the project. This component is used to display a simple explanation in the personal tutor feature of the user interface.

---
+++ src/components/Dashboard/PersonalTutor/GiveSimpleExplanation/GiveSimpleExplanation.js
@@ -0,0 +1,24 @@
+import react, { Component } from "react";
+
+class GiveSimpleExplanation extends Component {
+  render() {
+    return (
+      <div>
+        <div className="text-[3rem] font-[700] font-SFPro leading-[3.58rem]">
+          Real Life Example
+        </div>
+        <div className="text-[2rem] font-[400] font-SFPro leading-[2.387rem]">
+          Imagine a dynamic scene where a runner is sprinting along a track. The
+          runner's body is slightly blurred to emphasize the speed and movement,
+          with motion lines trailing behind to further illustrate the fast
+          motion. The background shows a track with marked lanes and a blurred
+          audience, creating a sense of rapid movement. The runner's limbs are
+          extended in mid-stride, with one foot barely touching the ground,
+          capturing the essence of motion and energy.
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GiveSimpleExplanation;
