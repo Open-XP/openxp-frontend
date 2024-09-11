c8774a80da62709d67a5cfbd5fa17a703d06ccab
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-09-11 10:42:08 +0100

    Refactor AI actions and add new functionality

--- src/Actions/AI.js
+++ src/Actions/AI.js
@@ -31,6 +31,16 @@ import {
   FLUSH_ALL_AI_FAIL,
   GENERATED_DATAILED_PERSONALIZE_NOTE_SUCCESS,
   GENERATED_DETAILED_PERSONALIZE_NOTE_FAIL,
+  PERSONALIZED_TEST_GENERATED_SUCCESSS,
+  PERSONALIZED_TEST_GENERATED_FAIL,
+  FETCH_GENERATED_PERSONALIZED_TEST_SUCCESS,
+  FETCH_GENERATED_PERSONALIZED_TEST_FAIL,
+  SELECTED_ANSWER_SUCCESS,
+  SELECTED_ANSWER_FAIL,
+  SIMULATED_TEST_COMPLETED_SUCCESS,
+  SIMULATED_TEST_COMPLETED_FAIL,
+  FETCH_SIMULATED_USER_TEST_SCORE_SUCCESS,
+  FETCH_SIMULATED_USER_TEST_SCORE_FAIL,
 } from "./Types";
 
 export const explainAnswer = (prompt) => (dispatch, getState) => {
@@ -277,3 +287,116 @@ export const GenerateDetailedPersonalizedNotes =
         dispatch({ type: GENERATED_DETAILED_PERSONALIZE_NOTE_FAIL });
       });
   };
+
+export const generatePersonalizedTestQuestion =
+  (subject, topic, num_questions, difficulty) => async (dispatch, getState) => {
+    const body = JSON.stringify({
+      subject,
+      topic,
+      num_questions,
+      difficulty,
+    });
+    try {
+      const res = await axios.post(
+        `/api/ai/generate-questions-create-test/`,
+        body,
+        tokenConfig(getState)
+      );
+      dispatch({
+        type: PERSONALIZED_TEST_GENERATED_SUCCESSS,
+        payload: res.data,
+      });
+      return res.data;
+    } catch (err) {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({ type: PERSONALIZED_TEST_GENERATED_FAIL });
+      throw err;
+    }
+  };
+
+export const fetchGeneratedPersonalizedTestQuestions =
+  (id) => (dispatch, getState) => {
+    return axios
+      .get(`/api/ai/generated-test-instance/${id}/`, tokenConfig(getState))
+      .then((res) =>
+        dispatch({
+          type: FETCH_GENERATED_PERSONALIZED_TEST_SUCCESS,
+          payload: res.data,
+        })
+      )
+      .catch((err) => {
+        dispatch(returnErrors(err.response.data, err.response.status));
+        dispatch({ type: FETCH_GENERATED_PERSONALIZED_TEST_FAIL });
+        throw err;
+      });
+  };
+
+export const submitPersonalizedTestAnswers =
+  (test_instance_id, question_id, selected_option) => (dispatch, getState) => {
+    const body = JSON.stringify({
+      test_instance: test_instance_id,
+      question: question_id,
+      selected_option: selected_option,
+    });
+    axios
+      .post(`/api/ai/submit-answer/`, body, tokenConfig(getState))
+      .then((res) =>
+        dispatch({
+          type: SELECTED_ANSWER_SUCCESS,
+          payload: res.data,
+        })
+      )
+      .catch((err) => {
+        dispatch(returnErrors(err.response.data, err.response.status));
+        dispatch({ type: SELECTED_ANSWER_FAIL });
+      });
+  };
+
+export const completeSimulatedTest =
+  (test_instance_id) => (dispatch, getState) => {
+    const requestBody = JSON.stringify({
+      test_instance_id,
+    });
+
+    axios
+      .post(
+        `/api/ai/complete-simulated-test/`,
+        requestBody,
+        tokenConfig(getState)
+      )
+      .then((res) => {
+        dispatch({
+          type: SIMULATED_TEST_COMPLETED_SUCCESS,
+          payload: res.data,
+        });
+      })
+      .catch((err) => {
+        dispatch(returnErrors(err.response.data, err.response.status));
+        dispatch({
+          type: SIMULATED_TEST_COMPLETED_FAIL,
+        });
+      });
+  };
+
+export const fetchSimulatedTestUserScore = (id) => (dispatch, getState) => {
+  axios
+    .get(`/api/ai/fetch-test-results/${id}/`, tokenConfig(getState))
+    .then((res) =>
+      dispatch({
+        type: FETCH_SIMULATED_USER_TEST_SCORE_SUCCESS,
+        payload: res.data, // Assuming you'll want to use the data returned from the response
+      })
+    )
+    .catch((err) => {
+      // Check if err.response exists before trying to access err.response.data
+      if (err.response) {
+        dispatch(returnErrors(err.response.data, err.response.status));
+      } else {
+        dispatch(
+          returnErrors({ message: "An unexpected error occurred" }, 500)
+        );
+      }
+      dispatch({ type: FETCH_SIMULATED_USER_TEST_SCORE_FAIL });
+      throw err; // If you need to propagate the error
+    });
+};
