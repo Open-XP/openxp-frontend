3795108b90126c9fe36c583784cdc2e48a522a4b
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-09-16 22:35:00 +0100

    Refactor SimpleExplanationLoader component and add animation styles

--- src/components/Dashboard/PersonalTutor/SimpleExplanationLoader/SimpleExplanationLoader.js
+++ src/components/Dashboard/PersonalTutor/SimpleExplanationLoader/SimpleExplanationLoader.js
@@ -1,5 +1,6 @@
-import react, { Component } from "react";
+import React, { Component } from "react";
 import OpenxpSVG from "../../../../svgs/openxp";
+import "./SimpleExplanationLoader.css"; // Animation styles
 
 class SimpleExplanationLoader extends Component {
   render() {
@@ -7,12 +8,12 @@ class SimpleExplanationLoader extends Component {
       <div>
         <div className="flex w-[13.75rem] items-center gap-[1rem]">
           <OpenxpSVG className="w-[1.813rem] h-[2.063rem]" />
-          <div className="size-[11px] bg-[#D9D9D9] rounded-[50%]"></div>
-          <div className="size-[11px] bg-[#D9D9D9] rounded-[50%]"></div>
-          <div className="size-[11px] bg-[#D9D9D9] rounded-[50%]"></div>
-          <div className="size-[11px] bg-[#D9D9D9] rounded-[50%]"></div>
-          <div className="size-[11px] bg-[#D9D9D9] rounded-[50%]"></div>
-          <div className="size-[11px] bg-[#D9D9D9] rounded-[50%]"></div>
+          <div className="circle bg-[#D9D9D9] animation-delay-1"></div>
+          <div className="circle bg-[#D9D9D9] animation-delay-2"></div>
+          <div className="circle bg-[#D9D9D9] animation-delay-3"></div>
+          <div className="circle bg-[#D9D9D9] animation-delay-4"></div>
+          <div className="circle bg-[#D9D9D9] animation-delay-5"></div>
+          <div className="circle bg-[#D9D9D9] animation-delay-6"></div>
         </div>
       </div>
     );
